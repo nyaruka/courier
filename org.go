22fb67a7c48e70c2ebd9f8a71476ba1e8e1a5b51
Nic Pottier <nicpottier@gmail.com>
2017-05-11 14:01:09 -0500

    missing files

---
+++ org.go
@@ -0,0 +1,11 @@
+package courier
+
+import "database/sql"
+
+// OrgID is our type for database Org ids
+type OrgID struct {
+	sql.NullInt64
+}
+
+// NilOrgID is our nil value for OrgID
+var NilOrgID = OrgID{sql.NullInt64{Int64: 0, Valid: false}}
