5b50c0ca798ac8238322894346a58bdf744cc019
Nic Pottier <nicpottier@gmail.com>
2017-08-22 14:36:01 -0500

    optimize lookup of dupe sends, clear sent status on error status

--- backends/rapidpro/backend_test.go
+++ backends/rapidpro/backend_test.go
@@ -372,13 +372,22 @@ func (ts *MsgTestSuite) TestOutgoingQueue() {
 	ts.True(sent)
 
 	// pop another message off, shouldn't get anything
-	msg, err = ts.b.PopNextOutgoingMsg()
-	ts.Nil(msg)
+	msg2, err := ts.b.PopNextOutgoingMsg()
+	ts.Nil(msg2)
 	ts.Nil(err)
 
 	// checking another message should show unsent
-	msg, err = readMsgFromDB(ts.b, courier.NewMsgID(10001))
+	msg3, err := readMsgFromDB(ts.b, courier.NewMsgID(10001))
 	ts.NoError(err)
+	sent, err = ts.b.WasMsgSent(msg3)
+	ts.NoError(err)
+	ts.False(sent)
+
+	// write an error for our original message
+	err = ts.b.WriteMsgStatus(ts.b.NewMsgStatusForID(msg.Channel(), msg.ID(), courier.MsgErrored))
+	ts.NoError(err)
+
+	// message should no longer be considered sent
 	sent, err = ts.b.WasMsgSent(msg)
 	ts.NoError(err)
 	ts.False(sent)
