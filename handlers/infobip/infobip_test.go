78955bf652a0cf7ac297c129493b7640f2668bda
Norbert Kwizera <norkans7@gmail.com>
2017-11-15 17:34:29 +0200

    Update test label

--- handlers/infobip/infobip_test.go
+++ handlers/infobip/infobip_test.go
@@ -84,7 +84,7 @@ var testCases = []ChannelHandleTestCase{
 	{Label: "Receive Valid Message", URL: receiveURL, Data: helloMsg, Status: 200, Response: "Accepted",
 		Text: Sp("QUIZ Correct answer is Paris"), URN: Sp("tel:+385916242493"), External: Sp("817790313235066447"), Date: Tp(time.Date(2016, 10, 06, 9, 28, 39, 220000000, time.UTC))},
 	{Label: "Receive missing results key", URL: receiveURL, Data: missingResults, Status: 400, Response: "validation for 'Results' failed"},
-	{Label: "Receive missing results key", URL: receiveURL, Data: missingText, Status: 400, Response: "validation for 'Text' failed"},
+	{Label: "Receive missing text key", URL: receiveURL, Data: missingText, Status: 400, Response: "validation for 'Text' failed"},
 }
 
 func TestHandler(t *testing.T) {
