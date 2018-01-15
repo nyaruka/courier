3b1cb78ee4fc879190ca146aa6252dec1fad724f
Nic Pottier <nicpottier@gmail.com>
2018-01-15 12:33:56 -0800

    add unicode test

--- handlers/start/start_test.go
+++ handlers/start/start_test.go
@@ -2,12 +2,11 @@ package start
 
 import (
 	"net/http/httptest"
-	"time"
 	"testing"
-	
+	"time"
+
 	"github.com/nyaruka/courier"
 	. "github.com/nyaruka/courier/handlers"
-
 )
 
 var testChannels = []courier.Channel{
@@ -16,7 +15,7 @@ var testChannels = []courier.Channel{
 
 var (
 	receiveURL = "/c/st/8eb23e93-5ecb-45ba-b726-3b064e0c56ab/receive/"
-	
+
 	notXML = "empty"
 
 	validReceive = `<message>
@@ -25,6 +24,13 @@ var (
 	<to>1515</to>
 	<body content-type="content-type" encoding="utf8">Hello World</body>
 	</message>`
+
+	validReceiveEncoded = "<message>" +
+		"<service type='sms' timestamp='1450450974' auth='10c5cfa4d8111e8523681fdbacd32d0b' request_id='43473486'/>" +
+		"<from>380501529999</from>" +
+		"<to>4224</to>" +
+		"<body>\xD0\x9A\xD0\xBE\xD1\x85\xD0\xB0\xD0\xBD\xD0\xBD\xD1\x8F</body>" +
+		"</message>`"
 
 	validReceiveEmptyText = `<message>
 	<service type="sms" timestamp="1450450974" auth="asdfasdf" request_id="msg1"/>
@@ -57,12 +63,13 @@ var (
 	<from>+250788123123</from>
 	<to>1515</to>
 	</message>`
-
 )
 
 var testCases = []ChannelHandleTestCase{
 	{Label: "Receive Valid", URL: receiveURL, Data: validReceive, Status: 200, Response: "Message Accepted",
 		Text: Sp("Hello World"), URN: Sp("tel:+250788123123"), Date: Tp(time.Date(2015, 12, 18, 15, 02, 54, 0, time.UTC))},
+	{Label: "Receive Valid Encoded", URL: receiveURL, Data: validReceiveEncoded, Status: 200, Response: "Message Accepted",
+		Text: Sp("Кохання"), URN: Sp("tel:+380501529999"), Date: Tp(time.Date(2015, 12, 18, 15, 02, 54, 0, time.UTC))},
 	{Label: "Receive Valid with empty Text", URL: receiveURL, Data: validReceiveEmptyText, Status: 200, Response: "Message Accepted",
 		Text: Sp(""), URN: Sp("tel:+250788123123")},
 	{Label: "Receive Valid missing body", URL: receiveURL, Data: validMissingBody, Status: 200, Response: "Message Accepted",
@@ -71,7 +78,6 @@ var testCases = []ChannelHandleTestCase{
 	{Label: "Receive missing From", URL: receiveURL, Data: missingFrom, Status: 400, Response: "Error"},
 	{Label: "Receive missing To", URL: receiveURL, Data: missingTo, Status: 400, Response: "Error"},
 	{Label: "Invalid XML", URL: receiveURL, Data: notXML, Status: 400, Response: "Error"},
-
 }
 
 func TestHandler(t *testing.T) {
@@ -82,7 +88,7 @@ func BenchmarkHandler(b *testing.B) {
 	RunChannelBenchmarks(b, testChannels, NewHandler(), testCases)
 }
 
- // setSendURL takes care of setting the sendURL to call
+// setSendURL takes care of setting the sendURL to call
 func setSendURL(server *httptest.Server, channel courier.Channel, msg courier.Msg) {
 	sendURL = server.URL
 }
@@ -116,7 +122,7 @@ var defaultSendTestCases = []ChannelSendTestCase{
 		SendPrep:    setSendURL},
 	{Label: "Send Attachment",
 		Text:           "My pic!",
-		Attachments: []string{"image/jpeg:https://foo.bar/image.jpg"},
+		Attachments:    []string{"image/jpeg:https://foo.bar/image.jpg"},
 		URN:            "tel:+250788383383",
 		Status:         "W",
 		ExternalID:     "380502535130309161501",
@@ -150,12 +156,11 @@ var defaultSendTestCases = []ChannelSendTestCase{
 			"Authorization": "Basic VXNlcm5hbWU6UGFzc3dvcmQ=",
 		},
 		RequestBody: `<message><service id="single" source="2020" validity="+12 hours"></service><to>+250788383383</to><body content-type="plain/text" encoding="plain">Error Message</body></message>`,
-		SendPrep:   setSendURL},
-	}
+		SendPrep:    setSendURL},
+}
 
 func TestSending(t *testing.T) {
 	maxMsgLength = 160
 	var defaultChannel = courier.NewMockChannel("8eb23e93-5ecb-45ba-b726-3b064e0c56ab", "ST", "2020", "UA", map[string]interface{}{"username": "Username", "password": "Password"})
 	RunChannelSendTestCases(t, defaultChannel, NewHandler(), defaultSendTestCases)
 }
-
