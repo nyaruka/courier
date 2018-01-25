ea7dc1a26340c117ab473c1e7604d7b69eba549e
Nic Pottier <nicpottier@gmail.com>
2018-01-25 14:06:23 -0800

    make it compile

--- handlers/twitter/twitter.go
+++ handlers/twitter/twitter.go
@@ -1,25 +1,18 @@
 package facebook
 
 import (
-	"bytes"
 	"context"
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/base64"
-	"encoding/json"
 	"fmt"
 	"net/http"
-	"net/url"
 	"strconv"
-	"strings"
 	"time"
 
-	"github.com/buger/jsonparser"
 	"github.com/nyaruka/courier"
 	"github.com/nyaruka/courier/handlers"
-	"github.com/nyaruka/courier/utils"
 	"github.com/nyaruka/gocommon/urns"
-	"github.com/pkg/errors"
 )
 
 // Endpoints we hit
@@ -214,14 +207,12 @@ type mtEnvelope struct {
 	} `json:"event"`
 }
 
-type mtQR {
-				QuickReply struct {
-					Type    string `json:"type"`
-					Options []mtQROption {
-						Label string `json:"label"`
-					} `json:"options"`
+type mtQR struct {
+	QuickReply struct {
+		Type    string       `json:"type"`
+		Options []mtQROption `json:"options"`
+	}
 }
-
 
 type mtQROption struct {
 	Title       string `json:"title"`
@@ -230,133 +221,7 @@ type mtQROption struct {
 }
 
 func (h *handler) SendMsg(ctx context.Context, msg courier.Msg) (courier.MsgStatus, error) {
-	// can't do anything without an access token
-	accessToken := msg.Channel().StringConfigForKey(courier.ConfigAuthToken, "")
-	if accessToken == "" {
-		return nil, fmt.Errorf("missing access token")
-	}
-
-	payload := mtEnvelope{}
-
-	// set our message type
-	if msg.ResponseToID().IsZero() {
-		payload.MessagingType = "NON_PROMOTIONAL_SUBSCRIPTION"
-	} else {
-		payload.MessagingType = "RESPONSE"
-	}
-
-	// build our recipient
-	if msg.URN().IsFacebookRef() {
-		payload.Recipient.UserRef = msg.URN().FacebookRef()
-	} else {
-		payload.Recipient.ID = msg.URN().Path()
-	}
-
-	sendURL, _ := url.Parse(facebookSendURL)
-	query := url.Values{}
-	query.Set("access_token", accessToken)
-	sendURL.RawQuery = query.Encode()
-
-	status := h.Backend().NewMsgStatusForID(msg.Channel(), msg.ID(), courier.MsgErrored)
-
-	msgParts := make([]string, 0)
-	if msg.Text() != "" {
-		msgParts = handlers.SplitMsg(msg.Text(), maxMsgLength)
-	}
-
-	// send each part and each attachment separately
-	for i := 0; i < len(msgParts)+len(msg.Attachments()); i++ {
-		if i < len(msgParts) {
-			// this is still a msg part
-			payload.Message.Text = msgParts[i]
-			payload.Message.Attachment = nil
-		} else {
-			// this is an attachment
-			payload.Message.Attachment = &mtAttachment{}
-			attType, attURL := courier.SplitAttachment(msg.Attachments()[i-len(msgParts)])
-			attType = strings.Split(attType, "/")[0]
-			payload.Message.Attachment.Type = attType
-			payload.Message.Attachment.Payload.URL = attURL
-			payload.Message.Attachment.Payload.IsReusable = true
-			payload.Message.Text = ""
-		}
-
-		// include any quick replies on the first piece we send
-		if i == 0 {
-			for _, qr := range msg.QuickReplies() {
-				payload.Message.QuickReplies = append(payload.Message.QuickReplies, mtQuickReply{qr, qr, "text"})
-			}
-		} else {
-			payload.Message.QuickReplies = nil
-		}
-
-		jsonBody, err := json.Marshal(payload)
-		if err != nil {
-			return status, err
-		}
-
-		req, err := http.NewRequest(http.MethodPost, sendURL.String(), bytes.NewReader(jsonBody))
-		req.Header.Set("Content-Type", "application/json")
-		req.Header.Set("Accept", "application/json")
-		rr, err := utils.MakeHTTPRequest(req)
-
-		// record our status and log
-		log := courier.NewChannelLogFromRR("Message Sent", msg.Channel(), msg.ID(), rr).WithError("Message Send Error", err)
-		status.AddLog(log)
-		if err != nil {
-			return status, nil
-		}
-
-		externalID, err := jsonparser.GetString(rr.Body, "message_id")
-		if err != nil {
-			log.WithError("Message Send Error", errors.Errorf("unable to get message_id from body"))
-			return status, nil
-		}
-
-		// if this is our first message, record the external id
-		if i == 0 {
-			status.SetExternalID(externalID)
-		}
-
-		// this was wired successfully
-		status.SetStatus(courier.MsgWired)
-	}
-
-	return status, nil
-}
-
-// ReceiveVerify handles Facebook's webhook verification callback
-func (h *handler) DescribeURN(ctx context.Context, channel courier.Channel, urn urns.URN) (map[string]string, error) {
-	// can't do anything with facebook refs, ignore them
-	if urn.IsFacebookRef() {
-		return map[string]string{}, nil
-	}
-
-	accessToken := channel.StringConfigForKey(courier.ConfigAuthToken, "")
-	if accessToken == "" {
-		return nil, fmt.Errorf("missing access token")
-	}
-
-	// build a request to lookup the stats for this contact
-	base, _ := url.Parse(facebookGraphURL)
-	path, _ := url.Parse(fmt.Sprintf("/%s", urn.Path()))
-	u := base.ResolveReference(path)
-
-	query := url.Values{}
-	query.Set("fields", "first_name,last_name")
-	query.Set("access_token", accessToken)
-	u.RawQuery = query.Encode()
-	req, _ := http.NewRequest(http.MethodGet, u.String(), nil)
-	rr, err := utils.MakeHTTPRequest(req)
-	if err != nil {
-		return nil, fmt.Errorf("unable to look up contact data:%s\n%s", err, rr.Response)
-	}
-
-	// read our first and last name
-	firstName, _ := jsonparser.GetString(rr.Body, "first_name")
-	lastName, _ := jsonparser.GetString(rr.Body, "last_name")
-
-	return map[string]string{"name": utils.JoinNonEmpty(" ", firstName, lastName)}, nil
+	return nil, fmt.Errorf("sending not implemented for twitter")
 }
 
 // hashes the passed in content in sha256 using the passed in secret
