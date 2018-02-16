b7071c5b4a6d73f41025c775b54706861a69533f
Norbert Kwizera <norkans7@gmail.com>
2018-02-16 16:45:59 +0200

    Add HX sending

--- handlers/highconnection/highconnection.go
+++ handlers/highconnection/highconnection.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"net/url"
 	"time"
 
 	"github.com/nyaruka/courier"
 	"github.com/nyaruka/courier/handlers"
+	"github.com/nyaruka/courier/utils"
 	"github.com/nyaruka/gocommon/urns"
 )
 
@@ -17,6 +19,9 @@ GET /handlers/hcnx/status/uuid?push_id=1164711372&status=6&to=%2B33611441111&ret
 POST /handlers/hcnx/receive/uuid?FROM=+33644961111
 ID=1164708294&FROM=%2B33644961111&TO=36105&MESSAGE=Msg&VALIDITY_DATE=2017-05-03T21%3A13%3A13&GET_STATUS=0&CLIENT=LEANCONTACTFAST&CLASS_TYPE=0&RECEPTION_DATE=2017-05-02T21%3A13%3A13&TO_OP_ID=20810&INITIAL_OP_ID=20810&STATUS=POSTING_30179_1410&EMAIL=&BINARY=0&PARAM=%7C%7C%7C%7CP223%2F03%2F03&USER_DATA=LEANCONTACTFAST&USER_DATA_2=jours+pas+r%E9gl%E9&BULK_ID=0&MO_ID=0&APPLICATION_ID=0&ACCOUNT_ID=39&GW_MESSAGE_ID=0&READ_STATUS=0&TARIFF=0&REQUEST_ID=33609002123&TAC=%28null%29&REASON=2017-05-02+23%3A13%3A13&FORMAT=&MVNO=&ORIG_ID=1164708215&ORIG_MESSAGE=Msg&RET_ID=123456&ORIG_DATE=2017-05-02T21%3A11%3A44
 */
+
+var sendURL = "https://highpushfastapi-v2.hcnx.eu/api"
+var maxMsgLength = 1500
 
 func init() {
 	courier.RegisterHandler(newHandler())
@@ -136,5 +141,52 @@ func (h *handler) StatusMessage(ctx context.Context, channel courier.Channel, w 
 
 // SendMsg sends the passed in message, returning any error
 func (h *handler) SendMsg(ctx context.Context, msg courier.Msg) (courier.MsgStatus, error) {
-	return nil, fmt.Errorf("HX sending via courier not yet implemented")
+	username := msg.Channel().StringConfigForKey(courier.ConfigUsername, "")
+	if username == "" {
+		return nil, fmt.Errorf("no username set for HX channel")
+	}
+
+	password := msg.Channel().StringConfigForKey(courier.ConfigPassword, "")
+	if password == "" {
+		return nil, fmt.Errorf("no password set for HX channel")
+	}
+
+	callbackDomain := msg.Channel().CallbackDomain(h.Server().Config().Domain)
+	statusURL := fmt.Sprintf("https://%s%s%s/status", callbackDomain, "/c/hx/", msg.Channel().UUID())
+	receiveURL := fmt.Sprintf("https://%s%s%s/receive", callbackDomain, "/c/hx/", msg.Channel().UUID())
+
+	status := h.Backend().NewMsgStatusForID(msg.Channel(), msg.ID(), courier.MsgErrored)
+	parts := handlers.SplitMsg(handlers.GetTextAndAttachments(msg), maxMsgLength)
+	for _, part := range parts {
+
+		form := url.Values{
+			"accountid":  []string{username},
+			"password":   []string{password},
+			"text":       []string{part},
+			"to":         []string{msg.URN().Path()},
+			"ret_id":     []string{msg.ID().String()},
+			"datacoding": []string{"8"},
+			"userdata":   []string{"textit"},
+			"ret_url":    []string{statusURL},
+			"ret_mo_url": []string{receiveURL},
+		}
+
+		msgURL, _ := url.Parse(sendURL)
+		msgURL.RawQuery = form.Encode()
+
+		req, err := http.NewRequest(http.MethodPost, msgURL.String(), nil)
+		rr, err := utils.MakeHTTPRequest(req)
+
+		// record our status and log
+		log := courier.NewChannelLogFromRR("Message Sent", msg.Channel(), msg.ID(), rr).WithError("Message Send Error", err)
+		status.AddLog(log)
+		if err != nil {
+			return status, nil
+		}
+
+		status.SetStatus(courier.MsgWired)
+
+	}
+
+	return status, nil
 }
--- handlers/highconnection/highconnection_test.go
+++ handlers/highconnection/highconnection_test.go
@@ -1,6 +1,7 @@
 package highconnection
 
 import (
+	"net/http/httptest"
 	"testing"
 	"time"
 
@@ -16,17 +17,19 @@ var (
 	receiveURL = "/c/hx/8eb23e93-5ecb-45ba-b726-3b064e0c56ab/receive/"
 	statusURL  = "/c/hx/8eb23e93-5ecb-45ba-b726-3b064e0c56ab/status/"
 
-	validReceive = receiveURL + "?FROM=+33610346460&TO=5151&MESSAGE=Hello+World&RECEPTION_DATE=2015-04-02T14:26:06"
-	validStatus  = statusURL + "?ret_id=12345&status=6"
+	validReceive       = receiveURL + "?FROM=+33610346460&TO=5151&MESSAGE=Hello+World&RECEPTION_DATE=2015-04-02T14:26:06"
+	invalidDateReceive = receiveURL + "?FROM=+33610346460&TO=5151&MESSAGE=Hello+World&RECEPTION_DATE=2015-04-02T14:26"
+	validStatus        = statusURL + "?ret_id=12345&status=6"
 )
 
 var testCases = []ChannelHandleTestCase{
 	{Label: "Receive Valid Message", URL: validReceive, Status: 200, Response: "Accepted",
 		Text: Sp("Hello World"), URN: Sp("tel:+33610346460"),
 		Date: Tp(time.Date(2015, 04, 02, 14, 26, 06, 0, time.UTC))},
-	{Label: "Receive missing params", URL: receiveURL, Status: 400, Response: "validation for 'Message' failed"},
-	{Label: "Status missing params", URL: statusURL, Status: 400, Response: "validation for 'Status' failed"},
-	{Label: "Status delivered", URL: validStatus, Status: 200, Response: `"status":"D"`},
+	{Label: "Receive Missing Params", URL: receiveURL, Status: 400, Response: "validation for 'Message' failed"},
+	{Label: "Receive Invalid Date", URL: invalidDateReceive, Status: 400, Response: "cannot parse"},
+	{Label: "Status Missing Params", URL: statusURL, Status: 400, Response: "validation for 'Status' failed"},
+	{Label: "Status Delivered", URL: validStatus, Status: 200, Response: `"status":"D"`},
 }
 
 func TestHandler(t *testing.T) {
@@ -35,4 +38,98 @@ func TestHandler(t *testing.T) {
 
 func BenchmarkHandler(b *testing.B) {
 	RunChannelBenchmarks(b, testChannels, newHandler(), testCases)
+}
+
+// setSend takes care of setting the sendURL to call
+func setSendURL(s *httptest.Server, h courier.ChannelHandler, c courier.Channel, m courier.Msg) {
+	sendURL = s.URL
+}
+
+var defaultSendTestCases = []ChannelSendTestCase{
+	{Label: "Plain Send",
+		Text:   "Simple Message",
+		URN:    "tel:+250788383383",
+		Status: "W",
+		URLParams: map[string]string{
+			"accountid":  "Username",
+			"password":   "Password",
+			"text":       "Simple Message",
+			"to":         "+250788383383",
+			"ret_id":     "10",
+			"datacoding": "8",
+			"userdata":   "textit",
+			"ret_url":    "https://localhost/c/hx/8eb23e93-5ecb-45ba-b726-3b064e0c56ab/status",
+			"ret_mo_url": "https://localhost/c/hx/8eb23e93-5ecb-45ba-b726-3b064e0c56ab/receive",
+		},
+		ResponseStatus: 200,
+		SendPrep:       setSendURL},
+	{Label: "Unicode Send",
+		Text:   "☺",
+		URN:    "tel:+250788383383",
+		Status: "W",
+		URLParams: map[string]string{
+			"accountid":  "Username",
+			"password":   "Password",
+			"text":       "☺",
+			"to":         "+250788383383",
+			"ret_id":     "10",
+			"datacoding": "8",
+			"userdata":   "textit",
+			"ret_url":    "https://localhost/c/hx/8eb23e93-5ecb-45ba-b726-3b064e0c56ab/status",
+			"ret_mo_url": "https://localhost/c/hx/8eb23e93-5ecb-45ba-b726-3b064e0c56ab/receive",
+		},
+		ResponseStatus: 200,
+		SendPrep:       setSendURL},
+	{Label: "Long Send",
+		Text:   "This is a longer message than 160 characters and will cause us to split it into two separate parts, isn't that right but it is even longer than before I say, I need to keep adding more things to make it work",
+		URN:    "tel:+250788383383",
+		Status: "W",
+		URLParams: map[string]string{
+			"accountid":  "Username",
+			"password":   "Password",
+			"text":       "I need to keep adding more things to make it work",
+			"to":         "+250788383383",
+			"ret_id":     "10",
+			"datacoding": "8",
+			"userdata":   "textit",
+			"ret_url":    "https://localhost/c/hx/8eb23e93-5ecb-45ba-b726-3b064e0c56ab/status",
+			"ret_mo_url": "https://localhost/c/hx/8eb23e93-5ecb-45ba-b726-3b064e0c56ab/receive",
+		},
+		ResponseStatus: 200,
+		SendPrep:       setSendURL},
+	{Label: "Send Attachement",
+		Text:        "My pic!",
+		Attachments: []string{"image/jpeg:https://foo.bar/image.jpg"},
+		URN:         "tel:+250788383383",
+		Status:      "W",
+		URLParams: map[string]string{
+			"accountid":  "Username",
+			"password":   "Password",
+			"text":       "My pic!\nhttps://foo.bar/image.jpg",
+			"to":         "+250788383383",
+			"ret_id":     "10",
+			"datacoding": "8",
+			"userdata":   "textit",
+			"ret_url":    "https://localhost/c/hx/8eb23e93-5ecb-45ba-b726-3b064e0c56ab/status",
+			"ret_mo_url": "https://localhost/c/hx/8eb23e93-5ecb-45ba-b726-3b064e0c56ab/receive",
+		},
+		ResponseStatus: 200,
+		SendPrep:       setSendURL},
+
+	{Label: "Error Sending",
+		Text: "Error Sending", URN: "tel:+250788383383",
+		Status:         "E",
+		ResponseStatus: 403,
+		SendPrep:       setSendURL},
+}
+
+func TestSending(t *testing.T) {
+	maxMsgLength = 160
+	var defaultChannel = courier.NewMockChannel("8eb23e93-5ecb-45ba-b726-3b064e0c56ab", "HX", "2020", "US",
+		map[string]interface{}{
+			courier.ConfigPassword: "Password",
+			courier.ConfigUsername: "Username",
+		})
+
+	RunChannelSendTestCases(t, defaultChannel, newHandler(), defaultSendTestCases, nil)
 }
