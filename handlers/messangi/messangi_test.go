2fe186c078842c73a9579d5c7d9eb3862609c3d1
John Cordeiro <john.dalton@ilhasoft.com.br>
2018-05-16 20:58:55 -0300

    Fix channel config keys on tests

--- handlers/messangi/messangi_test.go
+++ handlers/messangi/messangi_test.go
@@ -62,10 +62,10 @@ func TestSending(t *testing.T) {
 	maxMsgLength = 160
 	var defaultChannel = courier.NewMockChannel("8eb23e93-5ecb-45ba-b726-3b064e0c56ab", "MG", "2020", "JM",
 		map[string]interface{}{
-			"public_key": "my-public-key",
-			"private_key": "my-private-key",
-			"carrier_id": 7,
-			"instance_id": 2,
+			"messangi_public_key": "my-public-key",
+			"messangi_private_key": "my-private-key",
+			"messangi_instance_id": 7,
+			"messangi_carrier_id": 2,
 		})
 	RunChannelSendTestCases(t, defaultChannel, newHandler(), defaultSendTestCases, nil)
 }
