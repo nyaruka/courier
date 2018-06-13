eb6c6b458abc48aac1106e8ee7207378370f76c9
Nic Pottier <nicpottier@gmail.com>
2018-06-13 12:32:22 -0500

    add burst sms handler / sender

---
+++ handlers/burstsms/burstsms_test.go
@@ -0,0 +1,75 @@
+package burstsms
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	"github.com/nyaruka/courier"
+	. "github.com/nyaruka/courier/handlers"
+)
+
+var testChannels = []courier.Channel{
+	courier.NewMockChannel("8eb23e93-5ecb-45ba-b726-3b064e0c56ab", "BS", "2020", "US", nil),
+}
+
+var (
+	receiveURL = "/c/bs/8eb23e93-5ecb-45ba-b726-3b064e0c56ab/receive/"
+
+	validReceive  = "message=Msg&from=254791541111"
+	missingNumber = "message=Msg"
+)
+
+var testCases = []ChannelHandleTestCase{
+	{Label: "Receive Valid", URL: receiveURL, Data: validReceive, Status: 200, Response: "Message Accepted",
+		Text: Sp("Msg"), URN: Sp("tel:+254791541111")},
+	{Label: "Receive Missing Number", URL: receiveURL, Data: missingNumber, Status: 400, Response: "required field 'from'"},
+}
+
+func TestHandler(t *testing.T) {
+	RunChannelTestCases(t, testChannels, newHandler(), testCases)
+}
+
+func BenchmarkHandler(b *testing.B) {
+	RunChannelBenchmarks(b, testChannels, newHandler(), testCases)
+}
+
+func setSendURL(s *httptest.Server, h courier.ChannelHandler, c courier.Channel, m courier.Msg) {
+	sendURL = s.URL
+}
+
+var defaultSendTestCases = []ChannelSendTestCase{
+	{Label: "Plain Send",
+		Text: "Simple Message ☺", URN: "tel:+250788383383", Attachments: []string{"image/jpeg:https://foo.bar/image.jpg"},
+		Status: "W", ExternalID: "19835",
+		ResponseBody: `{ "message_id": 19835, "recipients": 3, "cost": 1.000 }`, ResponseStatus: 200,
+		PostParams: map[string]string{
+			"to":      "250788383383",
+			"message": "Simple Message ☺\nhttps://foo.bar/image.jpg",
+			"from":    "2020",
+		},
+		SendPrep: setSendURL},
+	{Label: "Invalid JSON",
+		Text: "Invalid JSON", URN: "tel:+250788383383",
+		Status:       "E",
+		ResponseBody: `not json`, ResponseStatus: 200,
+		SendPrep: setSendURL},
+	{Label: "Error Response",
+		Text: "Error Response", URN: "tel:+250788383383",
+		Status:       "F",
+		ResponseBody: `{ "message_id": 0 }`, ResponseStatus: 200,
+		SendPrep: setSendURL},
+	{Label: "Error Sending",
+		Text: "Error Message", URN: "tel:+250788383383",
+		Status:       "E",
+		ResponseBody: `Bad Gateway`, ResponseStatus: 501,
+		SendPrep: setSendURL},
+}
+
+func TestSending(t *testing.T) {
+	var defaultChannel = courier.NewMockChannel("8eb23e93-5ecb-45ba-b726-3b064e0c56ab", "BS", "2020", "US",
+		map[string]interface{}{
+			courier.ConfigUsername: "user1",
+			courier.ConfigPassword: "pass1",
+		})
+	RunChannelSendTestCases(t, defaultChannel, newHandler(), defaultSendTestCases, nil)
+}
