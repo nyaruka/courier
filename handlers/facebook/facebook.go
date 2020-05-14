37ea8199380c35c7206d88b1e7c2075f04603500
Norbert Kwizera <norkans7@gmail.com>
2020-05-14 15:47:36 +0200

    Upgrade FB graph API to 3.3

--- handlers/facebook/facebook.go
+++ handlers/facebook/facebook.go
@@ -21,9 +21,9 @@ import (
 
 // Endpoints we hit
 var (
-	sendURL      = "https://graph.facebook.com/v2.12/me/messages"
-	subscribeURL = "https://graph.facebook.com/v2.12/me/subscribed_apps"
-	graphURL     = "https://graph.facebook.com/v2.12/"
+	sendURL      = "https://graph.facebook.com/v3.3/me/messages"
+	subscribeURL = "https://graph.facebook.com/v3.3/me/subscribed_apps"
+	graphURL     = "https://graph.facebook.com/v3.3/"
 
 	// How long we want after the subscribe callback to register the page for events
 	subscribeTimeout = time.Second * 2
