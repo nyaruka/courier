a07a23c70e89788d3247dd268c8e3853afd4645f
Paul Bagyenda <bagyenda@dsmagic.com>
2020-09-20 13:15:35 +0300

    - Added ability to cleanup expired ussd request sessions

--- handlers/ussd/ussd.go
+++ handlers/ussd/ussd.go
@@ -1,6 +1,7 @@
 package ussd
 
 import (
+	"container/heap"
 	"context"
 	"github.com/nyaruka/courier"
 	"github.com/nyaruka/courier/handlers"
@@ -13,10 +14,12 @@ const (
 	configStartMsg    = "start_msg"
 	configTimeOut     = "time_out"
 	configStripPrefix = "strip_prefix"
+	SessionStatusWaiting     = "W"
+	ussdSessionTimeOut = 600 // 5 minutes
 )
 
 const (
-	SessionStatusWaiting     = "W"
+
 )
 func init() {
 	courier.RegisterHandler(newHandler())
@@ -27,16 +30,32 @@ type response struct {
 	wantResponse bool
 }
 
+type PqItem struct {
+	fkey string
+	index int
+	expiresOn time.Time
+}
+
+type sessions struct {
+	c chan response
+	i *PqItem
+}
+
 type handler struct {
 	handlers.BaseHandler
 
-	requests map[string]chan response // the request waiters, indexed by from+sessionID
+	requests  map[string] sessions  // map[string]chan response // the request waiters, indexed by from+sessionID
+
+	expirationQueue *PQueue // When last
 }
 
 func newHandler() courier.ChannelHandler {
+	pq := make(PQueue,0)
+	heap.Init(&pq) // init...
 	return &handler{
-		BaseHandler: handlers.NewBaseHandler(courier.ChannelType("US"), "USSD"),
-		requests:    make(map[string]chan response),
+		BaseHandler:      handlers.NewBaseHandler(courier.ChannelType("US"), "USSD"),
+		requests:         make(map[string]sessions),
+		expirationQueue: &pq,
 	}
 }
 
@@ -72,13 +91,23 @@ func (h *handler) receiveMessage(ctx context.Context, channel courier.Channel, w
 		return nil, handlers.WriteAndLogRequestError(ctx, h, channel, writer, request, err)
 	}
 	date := time.Now().UTC() // Current time...
+	var timeout = channel.IntConfigForKey(configTimeOut, 30)
+	var expiresOn = date.Add(time.Duration(ussdSessionTimeOut)*time.Second)
 	// build our msg
 	var input = form.Input
 
 	var fkey = makeKey(urn.Path(), form.ID) // Use canonical phone number in key...
-
-	if h.requests[fkey] == nil { // New session
-		h.requests[fkey] = make(chan response, 100) // For waiting for the response from rapidPro
+	r,ok := h.requests[fkey]
+	if !ok { // New session
+		r = sessions {
+			 c: make(chan response, 100), // For waiting for the response from rapidPro
+			 i: &PqItem{
+			 	fkey: fkey,
+			 	expiresOn: expiresOn,
+			 },
+		}
+		h.requests[fkey] = r
+		heap.Push(h.expirationQueue,r.i)
 		var smsg = channel.StringConfigForKey(configStartMsg, "")
 		if len(smsg) > 0 { // Use provided start message
 			input = smsg
@@ -92,31 +121,44 @@ func (h *handler) receiveMessage(ctx context.Context, channel courier.Channel, w
 				input = input[idx+1:] // Everything after the *
 			}
 		}
+		// update expires time.
+		h.expirationQueue.update(r.i,expiresOn)
 	}
 
 	msg := h.Backend().NewIncomingMsg(channel, urn, input).WithExternalID(form.ID).WithReceivedOn(date)
 	events, err := writeMsgs(ctx, h, []courier.Msg{msg})
 
 	// Now wait for the response from the  and send it back
-	var timeout = channel.IntConfigForKey(configTimeOut, 30)
 	var v string
 	var status int
 	select {
-	case res := <-h.requests[fkey]:
+	case res := <-r.c:
 		v = res.resp
 		if res.wantResponse {
 			status = http.StatusAccepted
+		} else {
+			heap.Remove(h.expirationQueue,r.i.index)
 		}
 	case <-time.After(time.Second * time.Duration(timeout)):
 		status = http.StatusGatewayTimeout
 		v = "time out waiting for response"
-		h.requests[fkey] = nil // Clear it. Right?
+		heap.Remove(h.expirationQueue,r.i.index) // Clear it. Right?
 	}
 
 	// Send HTTP response.
-	writer.Header().Set("Content-Type", "text/plain")
-	writer.WriteHeader(status)
-	_, err = writer.Write([]byte(v))
+	_, err = writeTextResponse(writer,status,v)
+
+	// Delete expired ones...
+	for h.expirationQueue.Len() > 0 {
+		item := heap.Pop(h.expirationQueue).(*PqItem)
+		if item.expiresOn.After(date) {
+			// put it back, we are done
+			heap.Push(h.expirationQueue,item)
+			break
+		}
+		// clear the session
+		delete( h.requests,item.fkey)
+	}
 	return events, err
 }
 
@@ -136,13 +178,13 @@ func (h *handler) SendMsg(ctx context.Context, msg courier.Msg) (courier.MsgStat
 
 	fkey := makeKey(sender, sessionID)
 
-	c := h.requests[fkey]
+	r,ok := h.requests[fkey]
 
 	status := h.Backend().NewMsgStatusForID(msg.Channel(), msg.ID(), courier.MsgFailed)
 
 	// Push out.
-	if c != nil {
-		c <- resp
+	if ok {
+		r.c <- resp
 		status.SetStatus(courier.MsgSent)
 	}
 	return status, nil
@@ -160,4 +202,44 @@ func writeMsgs(ctx context.Context, h handlers.ResponseWriter, msgs []courier.Ms
 	}
 
 	return events, nil
+}
+
+func writeTextResponse(w http.ResponseWriter, statusCode int, response string) (int,error) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(statusCode)
+	return w.Write([]byte(response))
+}
+
+// Priority queue used to ensure timed-out session do not stick around
+type PQueue []*PqItem
+func (pq PQueue) Len() int {return len(pq)}
+func (pq PQueue) Less (i, j int) bool {
+	return pq[i].expiresOn.Before(pq[j].expiresOn)
+}
+func (pq PQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*PqItem)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil  // avoid memory leak
+	item.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
+
+func (pq *PQueue) update(item *PqItem, expiresOn time.Time) {
+	item.expiresOn = expiresOn
+	heap.Fix(pq, item.index)
 }
