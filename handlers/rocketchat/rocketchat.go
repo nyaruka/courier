b06e7cf28a7e4e78f700c9f8f984e45c8d023382
Allan Lima <allancslima@gmail.com>
2020-10-06 09:02:46 -0300

    Add some tweaks to RocketChat handler

--- handlers/rocketchat/rocketchat.go
+++ handlers/rocketchat/rocketchat.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/buger/jsonparser"
 	"github.com/nyaruka/courier"
@@ -38,14 +39,19 @@ func (h *handler) Initialize(s courier.Server) error {
 	return nil
 }
 
+type Attachment struct {
+	Type string `json:"type"`
+	URL  string `json:"url"`
+}
+
 type moPayload struct {
 	User struct {
 		URN      string `json:"urn"     validate:"required"`
 		Username string `json:"username"`
 		FullName string `json:"full_name"`
 	} `json:"user" validate:"required"`
-	Text        string   `json:"text"        validate:"required"`
-	Attachments []string `json:"attachments"`
+	Text        string       `json:"text"`
+	Attachments []Attachment `json:"attachments"`
 }
 
 // receiveMessage is our HTTP handler function for incoming messages
@@ -62,6 +68,11 @@ func (h *handler) receiveMessage(ctx context.Context, channel courier.Channel, w
 		return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, err)
 	}
 
+	// check content empty
+	if payload.Text == "" && len(payload.Attachments) == 0 {
+		return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, errors.New("no text or attachment"))
+	}
+
 	urn, err := urns.NewURNFromParts(urns.RocketChatScheme, payload.User.URN, "", payload.User.Username)
 	if err != nil {
 		return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, err)
@@ -69,22 +80,17 @@ func (h *handler) receiveMessage(ctx context.Context, channel courier.Channel, w
 
 	msg := h.Backend().NewIncomingMsg(channel, urn, payload.Text).WithContactName(payload.User.FullName)
 	for _, attachment := range payload.Attachments {
-		msg.WithAttachment(attachment)
+		msg.WithAttachment(attachment.URL)
 	}
 
 	return handlers.WriteMsgsAndResponse(ctx, h, []courier.Msg{msg}, w, r)
 }
 
-type Attachment struct {
-	Type string `json:"type"`
-	URL  string `json:"url"`
-}
-
 type mtPayload struct {
-	UserURN     string       `json:"userURN"`
-	BotUsername string       `json:"botUsername"`
-	Text        string       `json:"text"`
-	Attachments []Attachment `json:"attachments"`
+	UserURN     string       `json:"user"`
+	BotUsername string       `json:"bot"`
+	Text        string       `json:"text,omitempty"`
+	Attachments []Attachment `json:"attachments,omitempty"`
 }
 
 func (h *handler) SendMsg(ctx context.Context, msg courier.Msg) (courier.MsgStatus, error) {
@@ -110,22 +116,27 @@ func (h *handler) SendMsg(ctx context.Context, msg courier.Msg) (courier.MsgStat
 		return status, err
 	}
 
-	req, err := http.NewRequest(http.MethodPost, baseURL + "/message", bytes.NewReader(body))
+	req, err := http.NewRequest(http.MethodPost, baseURL+"/message", bytes.NewReader(body))
 	if err != nil {
 		return status, err
 	}
+	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Authorization", fmt.Sprintf("Token %s", secret))
+
 	res, err := utils.MakeHTTPRequest(req)
-	if err != nil {
-		return status, err
-	}
+
 	log := courier.NewChannelLogFromRR("Message Sent", msg.Channel(), msg.ID(), res).WithError("Message Send Error", err)
 	status.AddLog(log)
-	status.SetStatus(courier.MsgSent)
+
+	if err != nil {
+		return status, err
+	}
 
 	msgID, err := jsonparser.GetString(res.Body, "id")
 	if err == nil {
 		status.SetExternalID(msgID)
 	}
+
+	status.SetStatus(courier.MsgSent)
 	return status, nil
 }
