5841ff1c304b0890285e4246d1cb9a2efa0f1911
Caio Almeida <cai0_almeida@outlook.com>
2021-03-24 17:40:40 -0300

    Implement receiveMsg to wwc

--- handlers/weniwebchat/weniwebchat.go
+++ handlers/weniwebchat/weniwebchat.go
@@ -1,2 +1,115 @@
 package weniwebchat
 
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/nyaruka/courier"
+	"github.com/nyaruka/courier/handlers"
+	"github.com/nyaruka/gocommon/urns"
+)
+
+func init() {
+	courier.RegisterHandler(newHandler())
+}
+
+type handler struct {
+	handlers.BaseHandler
+}
+
+func newHandler() courier.ChannelHandler {
+	return &handler{handlers.NewBaseHandler(courier.ChannelType("WWC"), "Weni Web Chat")}
+}
+
+// Initialize is called by the engine once everything is loaded
+func (h *handler) Initialize(s courier.Server) error {
+	h.SetServer(s)
+	s.AddHandlerRoute(h, http.MethodPost, "receive", h.receiveMsg)
+	return nil
+}
+
+// {
+// 	"type":"message",
+// 	"to":"<to>",
+// 	"from":"<from>",
+// 	"message":{
+// 	   "id":"<id>",
+// 	   "type": "text",
+// 	   "text": "<text>",
+// 	   "quick_replies":"<quick_replies>",
+// 	}
+// }
+
+type miPayload struct {
+	Type    string    `json:"type"           validate:"required"`
+	From    string    `json:"from,omitempty" validate:"required"`
+	Message miMessage `json:"message"`
+}
+
+type miMessage struct {
+	ID        string `json:"id"            validate:"required"`
+	Type      string `json:"type"          validate:"required"`
+	TimeStamp string `json:"timestamp"     validate:"required"`
+	Text      string `json:"text,omitempty"`
+	MediaURL  string `json:"media_url,omitempty"`
+	Caption   string `json:"caption,omitempty"`
+	Latitude  string `json:"latitude,omitempty"`
+	Longitude string `json:"longitude,omitempty"`
+}
+
+func (h *handler) receiveMsg(ctx context.Context, channel courier.Channel, w http.ResponseWriter, r *http.Request) ([]courier.Event, error) {
+	payload := &miPayload{}
+	err := handlers.DecodeAndValidateJSON(payload, r)
+	if err != nil {
+		return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, err)
+	}
+
+	// check message type
+	if payload.Type != "message" || (payload.Message.Type != "text" && payload.Message.Type != "image" && payload.Message.Type != "video" && payload.Message.Type != "voice" && payload.Message.Type != "document" && payload.Message.Type != "location") {
+		return nil, handlers.WriteAndLogRequestIgnored(ctx, h, channel, w, r, "ignoring request, unknown message type")
+	}
+
+	// check empty content
+	if payload.Message.Text == "" && payload.Message.MediaURL == "" && (payload.Message.Latitude == "" || payload.Message.Longitude == "") {
+		return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, errors.New("blank message, media or location"))
+	}
+
+	// build urn
+	urn, err := urns.NewURNFromParts(urns.ExternalScheme, payload.From, "", "")
+	if err != nil {
+		return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, err)
+	}
+
+	// parse timestamp
+	ts, err := strconv.ParseInt(payload.Message.TimeStamp, 10, 64)
+	if err != nil {
+		return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, fmt.Errorf("invalid timestamp: %s", payload.Message.TimeStamp))
+	}
+
+	// parse medias
+	var mediaURL string
+	if payload.Message.Type == "location" {
+		mediaURL = fmt.Sprintf("geo:%s,%s", payload.Message.Latitude, payload.Message.Longitude)
+	} else if payload.Message.MediaURL != "" {
+		mediaURL = payload.Message.MediaURL
+		payload.Message.Text = payload.Message.Caption
+	}
+
+	// build message
+	date := time.Unix(ts, 0).UTC()
+	msg := h.Backend().NewIncomingMsg(channel, urn, payload.Message.Text).WithReceivedOn(date).WithContactName(payload.From)
+
+	if mediaURL != "" {
+		msg.WithAttachment(mediaURL)
+	}
+
+	return handlers.WriteMsgsAndResponse(ctx, h, []courier.Msg{msg}, w, r)
+}
+
+func (h *handler) SendMsg(ctx context.Context, msg courier.Msg) (courier.MsgStatus, error) {
+	return nil, nil
+}
--- handlers/weniwebchat/weniwebchat_test.go
+++ handlers/weniwebchat/weniwebchat_test.go
@@ -1,36 +1,141 @@
 package weniwebchat
 
 import (
-	"context"
-	"net/http"
+	"fmt"
+	"testing"
 
 	"github.com/nyaruka/courier"
-	"github.com/nyaruka/courier/handlers"
+	. "github.com/nyaruka/courier/handlers"
 )
 
-func init() {
-	courier.RegisterHandler(newHandler())
+const channelUUID = "8eb23e93-5ecb-45ba-b726-3b064e0c568c"
+
+var receiveURL = fmt.Sprintf("/c/wwc/%s/receive", channelUUID)
+
+var testChannels = []courier.Channel{
+	courier.NewMockChannel(channelUUID, "WWC", "250788383383", "", map[string]interface{}{}),
 }
 
-type handler struct {
-	handlers.BaseHandler
+const (
+	textMsgTemplate = `
+	{
+		"type":"message",
+		"from":%q,
+		"message":{
+			"id":%q,
+			"type":"text",
+			"timestamp":%q,
+			"text":%q
+		}
+	}
+	`
+
+	imgMsgTemplate = `
+	{
+		"type":"message",
+		"from":%q,
+		"message":{
+			"id":%q,
+			"type":"image",
+			"timestamp":%q,
+			"media_url":%q,
+			"caption":%q
+		}
+	}
+	`
+
+	locationMsgTemplate = `
+	{
+		"type":"message",
+		"from":%q,
+		"message":{
+			"id":%q,
+			"timestamp":%q,
+			"type":"location",
+			"latitude":%q,
+			"longitude":%q
+		}
+	}
+	`
+
+	invalidMsgTemplate = `
+	{
+		"type":"foo",
+		"from":"bar",
+		"message": {
+			"id":"000001",
+			"type":"text",
+			"timestamp":"1616586927"
+		}
+	}
+	`
+)
+
+var testCases = []ChannelHandleTestCase{
+	{
+		Label:    "Receive Valid Text Msg",
+		URL:      receiveURL,
+		Data:     fmt.Sprintf(textMsgTemplate, "2345678", "12351236", "1616586927", "Hello Test!"),
+		Name:     Sp("2345678"),
+		URN:      Sp("ext:2345678"),
+		Text:     Sp("Hello Test!"),
+		Status:   200,
+		Response: "Accepted",
+	},
+	{
+		Label:      "Receive Valid Media",
+		URL:        receiveURL,
+		Data:       fmt.Sprintf(imgMsgTemplate, "2345678", "12351236", "1616586927", "https://link.to/image.png", "My Caption"),
+		Name:       Sp("2345678"),
+		URN:        Sp("ext:2345678"),
+		Text:       Sp("My Caption"),
+		Attachment: Sp("https://link.to/image.png"),
+		Status:     200,
+		Response:   "Accepted",
+	},
+	{
+		Label:      "Receive Valid Location",
+		URL:        receiveURL,
+		Data:       fmt.Sprintf(locationMsgTemplate, "2345678", "12351236", "1616586927", "-9.6996104", "-35.7794614"),
+		Name:       Sp("2345678"),
+		URN:        Sp("ext:2345678"),
+		Attachment: Sp("geo:-9.6996104,-35.7794614"),
+		Status:     200,
+		Response:   "Accepted",
+	},
+	{
+		Label:  "Receive Invalid JSON",
+		URL:    receiveURL,
+		Data:   "{}",
+		Status: 400,
+	},
+	{
+		Label:    "Receive Text Msg With Blank Message Text",
+		URL:      receiveURL,
+		Data:     fmt.Sprintf(textMsgTemplate, "2345678", "12351236", "1616586927", ""),
+		Status:   400,
+		Response: "blank message, media or location",
+	},
+	{
+		Label:    "Receive Invalid Timestamp",
+		URL:      receiveURL,
+		Data:     fmt.Sprintf(textMsgTemplate, "2345678", "12351236", "foo", "Hello Test!"),
+		Status:   400,
+		Response: "invalid timestamp: foo",
+	},
+	{
+		Label:    "Receive Invalid Message",
+		URL:      receiveURL,
+		Data:     invalidMsgTemplate,
+		Status:   200,
+		Response: "ignoring request, unknown message type",
+	},
 }
 
-func newHandler() courier.ChannelHandler {
-	return &handler{handlers.NewBaseHandler(courier.ChannelType("WWC"), "Weni Web Chat")}
+func TestHandler(t *testing.T) {
+	RunChannelTestCases(t, testChannels, newHandler(), testCases)
 }
 
-// Initialize is called by the engine once everything is loaded
-func (h *handler) Initialize(s courier.Server) error {
-	h.SetServer(s)
-	s.AddHandlerRoute(h, http.MethodGet, "receive", h.receiveMsg)
-	return nil
-}
-
-func (h *handler) receiveMsg(ctx context.Context, channel courier.Channel, w http.ResponseWriter, r *http.Request) ([]courier.Event, error) {
-	return nil, nil
-}
-
-func (h *handler) SendMsg(ctx context.Context, msg courier.Msg) (courier.MsgStatus, error) {
-	return nil, nil
+func BenchmarkHandler(b *testing.B) {
+	RunChannelBenchmarks(b, testChannels, newHandler(), testCases)
 }
