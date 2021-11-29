3233f3d7b0b229e8ae7ec700d4f7d64a1b3b9bbe
Robi9 <robertasousa714@gmail.com>
2021-11-29 10:51:22 -0300

    Add instagram handler

---
+++ handlers/instagram/instagram.go
@@ -0,0 +1,600 @@
+package instagram
+
+import (
+	"bytes"
+	"context"
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/buger/jsonparser"
+	"github.com/nyaruka/courier"
+	"github.com/nyaruka/courier/handlers"
+	"github.com/nyaruka/courier/utils"
+	"github.com/nyaruka/gocommon/urns"
+	"github.com/pkg/errors"
+)
+
+// Endpoints we hit
+var (
+	sendURL  = "https://graph.facebook.com/v12.0/me/messages"
+	graphURL = "https://graph.facebook.com/v12.0/"
+
+	signatureHeader = "X-Hub-Signature"
+
+	// max for the body
+	maxMsgLength = 1000
+
+	//Only Human_Agent tag available for instagram
+	tagByTopic = map[string]string{
+		"agent": "HUMAN_AGENT",
+	}
+)
+
+// keys for extra in channel events
+const (
+	titleKey   = "title"
+	payloadKey = "payload"
+)
+
+func init() {
+	courier.RegisterHandler(newHandler())
+}
+
+type handler struct {
+	handlers.BaseHandler
+}
+
+func newHandler() courier.ChannelHandler {
+	return &handler{handlers.NewBaseHandlerWithParams(courier.ChannelType("IG"), "Instagram", false)}
+}
+
+// Initialize is called by the engine once everything is loaded
+func (h *handler) Initialize(s courier.Server) error {
+	h.SetServer(s)
+	s.AddHandlerRoute(h, http.MethodGet, "receive", h.receiveVerify)
+	s.AddHandlerRoute(h, http.MethodPost, "receive", h.receiveEvent)
+	return nil
+}
+
+type igSender struct {
+	ID string `json:"id"`
+}
+
+type igUser struct {
+	ID string `json:"id"`
+}
+
+// {
+//   "object":"instagram",
+//   "entry":[{
+//     "id":"180005062406476",
+//     "time":1514924367082,
+//     "messaging":[{
+//       "sender":  {"id":"1630934236957797"},
+//       "recipient":{"id":"180005062406476"},
+//       "timestamp":1514924366807,
+//       "message":{
+//         "mid":"mid.$cAAD5QiNHkz1m6cyj11guxokwkhi2",
+//         "text":"65863634"
+//       }
+//     }]
+//   }]
+// }
+
+type moPayload struct {
+	Object string `json:"object"`
+	Entry  []struct {
+		ID        string `json:"id"`
+		Time      int64  `json:"time"`
+		Messaging []struct {
+			Sender    igSender `json:"sender"`
+			Recipient igUser   `json:"recipient"`
+			Timestamp int64    `json:"timestamp"`
+
+			Postback *struct {
+				MID     string `json:"mid"`
+				Title   string `json:"title"`
+				Payload string `json:"payload"`
+			} `json:"postback,omitempty"`
+
+			Message *struct {
+				IsEcho     bool   `json:"is_echo,omitempty"`
+				MID        string `json:"mid"`
+				Text       string `json:"text,omitempty"`
+				QuickReply struct {
+					Payload string `json:"payload"`
+				} `json:"quick_replies,omitempty"`
+				Attachments []struct {
+					Type    string `json:"type"`
+					Payload *struct {
+						URL string `json:"url"`
+					} `json:"payload"`
+				} `json:"attachments,omitempty"`
+			} `json:"message,omitempty"`
+		} `json:"messaging"`
+	} `json:"entry"`
+}
+
+/*type moPayload struct {
+	Object string `json:"object"`
+	Entry  []struct {
+		ID      string `json:"id"`
+		Time    int64  `json:"time"`
+		Changes []struct {
+			Field string `json:"field"`
+			Value struct {
+				Sender struct {
+					ID string `json:"id"`
+				} `json:"sender"`
+
+				Recipient struct {
+					ID string `json:"id"`
+				} `json:"recipient"`
+				Timestamp int64 `json:"timestamp"`
+
+				Postback *struct {
+					MID     string `json:"mid"`
+					Title   string `json:"title"`
+					Payload string `json:"payload"`
+				} `json:"postback,omitempty"`
+
+				Message *struct {
+					IsEcho     bool   `json:"is_echo,omitempty"`
+					MID        string `json:"mid"`
+					Text       string `json:"text,omitempty"`
+					QuickReply struct {
+						Payload string `json:"payload"`
+					} `json:"quick_replies,omitempty"`
+					Attachments []struct {
+						Type    string `json:"type"`
+						Payload *struct {
+							URL string `json:"url"`
+						} `json:"payload"`
+					} `json:"attachments,omitempty"`
+				} `json:"message,omitempty"`
+			} `json:"value"`
+		} `json:"changes"`
+	} `json:"entry"`
+}*/
+
+// GetChannel returns the channel
+func (h *handler) GetChannel(ctx context.Context, r *http.Request) (courier.Channel, error) {
+
+	if r.Method == http.MethodGet {
+
+		return nil, nil
+	}
+
+	payload := &moPayload{}
+
+	err := handlers.DecodeAndValidateJSON(payload, r)
+
+	if err != nil {
+
+		return nil, err
+	}
+
+	// not a instagram object? ignore
+	if payload.Object != "instagram" {
+
+		return nil, fmt.Errorf("object expected 'instagram', found %s", payload.Object)
+	}
+
+	// no entries? ignore this request
+	if len(payload.Entry) == 0 {
+
+		return nil, fmt.Errorf("no entries found")
+	}
+
+	igID := payload.Entry[0].ID
+
+	return h.Backend().GetChannelByAddress(ctx, courier.ChannelType("IG"), courier.ChannelAddress(igID))
+}
+
+// receiveVerify handles Instagram's webhook verification callback
+func (h *handler) receiveVerify(ctx context.Context, channel courier.Channel, w http.ResponseWriter, r *http.Request) ([]courier.Event, error) {
+	mode := r.URL.Query().Get("hub.mode")
+
+	// this isn't a subscribe verification, that's an error
+	if mode != "subscribe" {
+		return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, fmt.Errorf("unknown request"))
+	}
+
+	// verify the token against our server facebook webhook secret, if the same return the challenge IG sent us
+	secret := r.URL.Query().Get("hub.verify_token")
+
+	if secret != h.Server().Config().FacebookWebhookSecret {
+		return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, fmt.Errorf("token does not match secret"))
+	}
+	// and respond with the challenge token
+	_, err := fmt.Fprint(w, r.URL.Query().Get("hub.challenge"))
+	return nil, err
+}
+
+// receiveEvent is our HTTP handler function for incoming messages and status updates
+func (h *handler) receiveEvent(ctx context.Context, channel courier.Channel, w http.ResponseWriter, r *http.Request) ([]courier.Event, error) {
+	err := h.validateSignature(r)
+	if err != nil {
+		return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, err)
+	}
+
+	payload := &moPayload{}
+	err = handlers.DecodeAndValidateJSON(payload, r)
+	if err != nil {
+		return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, err)
+	}
+
+	// not a instagram object? ignore
+	if payload.Object != "instagram" {
+		return nil, handlers.WriteAndLogRequestIgnored(ctx, h, channel, w, r, "ignoring request")
+	}
+
+	// no entries? ignore this request
+	if len(payload.Entry) == 0 {
+		return nil, handlers.WriteAndLogRequestIgnored(ctx, h, channel, w, r, "ignoring request, no entries")
+	}
+
+	// the list of events we deal with
+	events := make([]courier.Event, 0, 2)
+
+	// the list of data we will return in our response
+	data := make([]interface{}, 0, 2)
+
+	// for each entry
+	for _, entry := range payload.Entry {
+		// no entry, ignore
+		if len(entry.Messaging) == 0 {
+			continue
+		}
+
+		// grab our message, there is always a single one
+		msg := entry.Messaging[0]
+
+		//msg.Value.Recipient.ID = "218041941572367"
+
+		// ignore this entry if it is to another page
+		if channel.Address() != msg.Recipient.ID {
+			continue
+		}
+
+		// create our date from the timestamp (they give us millis, arg is nanos)
+		date := time.Unix(0, msg.Timestamp*1000000).UTC()
+
+		sender := msg.Sender.ID
+		if sender == "" {
+			sender = msg.Sender.ID
+		}
+
+		// create our URN
+		urn, err := urns.NewInstagramURN(sender)
+		if err != nil {
+			return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, err)
+		}
+
+		if msg.Postback != nil {
+			// by default postbacks are treated as new conversations
+			eventType := courier.NewConversation
+			event := h.Backend().NewChannelEvent(channel, eventType, urn).WithOccurredOn(date)
+
+			// build our extra
+			extra := map[string]interface{}{
+				titleKey:   msg.Postback.Title,
+				payloadKey: msg.Postback.Payload,
+			}
+
+			event = event.WithExtra(extra)
+
+			err := h.Backend().WriteChannelEvent(ctx, event)
+			if err != nil {
+				return nil, err
+			}
+
+			events = append(events, event)
+			data = append(data, courier.NewEventReceiveData(event))
+		} else if msg.Message != nil {
+			// this is an incoming message
+			// ignore echos
+			if msg.Message.IsEcho {
+				data = append(data, courier.NewInfoData("ignoring echo"))
+				continue
+			}
+
+			text := msg.Message.Text
+
+			attachmentURLs := make([]string, 0, 2)
+
+			for _, att := range msg.Message.Attachments {
+				if att.Payload != nil && att.Payload.URL != "" {
+					attachmentURLs = append(attachmentURLs, att.Payload.URL)
+				}
+			}
+
+			// create our message
+			ev := h.Backend().NewIncomingMsg(channel, urn, text).WithExternalID(msg.Message.MID).WithReceivedOn(date)
+			event := h.Backend().CheckExternalIDSeen(ev)
+
+			// add any attachment URL found
+			for _, attURL := range attachmentURLs {
+				event.WithAttachment(attURL)
+			}
+
+			err := h.Backend().WriteMsg(ctx, event)
+			if err != nil {
+				return nil, err
+			}
+
+			h.Backend().WriteExternalIDSeen(event)
+
+			events = append(events, event)
+			data = append(data, courier.NewMsgReceiveData(event))
+
+		} else {
+			data = append(data, courier.NewInfoData("ignoring unknown entry type"))
+		}
+	}
+	return events, courier.WriteDataResponse(ctx, w, http.StatusOK, "Events Handled", data)
+}
+
+// {
+//     "messaging_type": "<MESSAGING_TYPE>"
+//     "recipient":{
+//         "id":"<PSID>"
+//     },
+//     "message":{
+//	       "text":"hello, world!"
+//         "attachment":{
+//             "type":"image",
+//             "payload":{
+//                 "url":"http://www.messenger-rocks.com/image.jpg",
+//                 "is_reusable":true
+//             }
+//         }
+//     }
+// }
+type mtPayload struct {
+	MessagingType string `json:"messaging_type"`
+	Tag           string `json:"tag,omitempty"`
+	Recipient     struct {
+		//UserRef string `json:"user_ref,omitempty"`
+		ID string `json:"id,omitempty"`
+	} `json:"recipient"`
+	Message struct {
+		Text         string         `json:"text,omitempty"`
+		QuickReplies []mtQuickReply `json:"quick_replies,omitempty"`
+		Attachment   *mtAttachment  `json:"attachment,omitempty"`
+	} `json:"message"`
+}
+
+type mtAttachment struct {
+	Type    string `json:"type"`
+	Payload struct {
+		URL        string `json:"url,omitempty"`
+		IsReusable bool   `json:"is_reusable,omitempty"`
+	} `json:"payload"`
+}
+type mtQuickReply struct {
+	Title       string `json:"title"`
+	Payload     string `json:"payload"`
+	ContentType string `json:"content_type"`
+}
+
+func (h *handler) SendMsg(ctx context.Context, msg courier.Msg) (courier.MsgStatus, error) {
+	// can't do anything without an access token
+	accessToken := msg.Channel().StringConfigForKey(courier.ConfigAuthToken, "")
+	if accessToken == "" {
+		return nil, fmt.Errorf("missing access token")
+	}
+
+	topic := msg.Topic()
+	payload := mtPayload{}
+
+	// set our message type
+	if msg.ResponseToID() != courier.NilMsgID {
+		payload.MessagingType = "RESPONSE"
+	} else if topic != "" {
+		payload.MessagingType = "MESSAGE_TAG"
+		payload.Tag = tagByTopic[topic]
+	} else {
+		payload.MessagingType = "UPDATE"
+	}
+
+	payload.Recipient.ID = msg.URN().Path()
+
+	msgURL, _ := url.Parse(sendURL)
+	query := url.Values{}
+	query.Set("access_token", accessToken)
+	msgURL.RawQuery = query.Encode()
+
+	status := h.Backend().NewMsgStatusForID(msg.Channel(), msg.ID(), courier.MsgErrored)
+
+	msgParts := make([]string, 0)
+	if msg.Text() != "" {
+		msgParts = handlers.SplitMsgByChannel(msg.Channel(), msg.Text(), maxMsgLength)
+	}
+
+	// send each part and each attachment separately. we send attachments first as otherwise quick replies
+	// attached to text messages get hidden when images get delivered
+	for i := 0; i < len(msgParts)+len(msg.Attachments()); i++ {
+		if i < len(msg.Attachments()) {
+			// this is an attachment
+			payload.Message.Attachment = &mtAttachment{}
+			attType, attURL := handlers.SplitAttachment(msg.Attachments()[i])
+			attType = strings.Split(attType, "/")[0]
+			payload.Message.Attachment.Type = attType
+			payload.Message.Attachment.Payload.URL = attURL
+			payload.Message.Attachment.Payload.IsReusable = true
+			payload.Message.Text = ""
+		} else {
+			// this is still a msg part
+			payload.Message.Text = msgParts[i-len(msg.Attachments())]
+			payload.Message.Attachment = nil
+		}
+
+		// include any quick replies on the last piece we send
+		if i == (len(msgParts)+len(msg.Attachments()))-1 {
+			for _, qr := range msg.QuickReplies() {
+				payload.Message.QuickReplies = append(payload.Message.QuickReplies, mtQuickReply{qr, qr, "text"})
+			}
+		} else {
+			payload.Message.QuickReplies = nil
+		}
+
+		jsonBody, err := json.Marshal(payload)
+		if err != nil {
+			return status, err
+		}
+
+		req, err := http.NewRequest(http.MethodPost, msgURL.String(), bytes.NewReader(jsonBody))
+		if err != nil {
+			return nil, err
+		}
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Accept", "application/json")
+
+		rr, err := utils.MakeHTTPRequest(req)
+
+		// record our status and log
+		log := courier.NewChannelLogFromRR("Message Sent", msg.Channel(), msg.ID(), rr).WithError("Message Send Error", err)
+		status.AddLog(log)
+		if err != nil {
+			return status, nil
+		}
+
+		externalID, err := jsonparser.GetString(rr.Body, "message_id")
+		if err != nil {
+			log.WithError("Message Send Error", errors.Errorf("unable to get message_id from body"))
+			return status, nil
+		}
+
+		// if this is our first message, record the external id
+		if i == 0 {
+			status.SetExternalID(externalID)
+			if msg.URN().IsInstagramRef() {
+				recipientID, err := jsonparser.GetString(rr.Body, "recipient_id")
+				if err != nil {
+					log.WithError("Message Send Error", errors.Errorf("unable to get recipient_id from body"))
+					return status, nil
+				}
+
+				referralID := msg.URN().InstagramRef()
+
+				realIDURN, err := urns.NewInstagramURN(recipientID)
+				if err != nil {
+					log.WithError("Message Send Error", errors.Errorf("unable to make Instagram urn from %s", recipientID))
+				}
+
+				contact, err := h.Backend().GetContact(ctx, msg.Channel(), msg.URN(), "", "")
+				if err != nil {
+					log.WithError("Message Send Error", errors.Errorf("unable to get contact for %s", msg.URN().String()))
+				}
+				realURN, err := h.Backend().AddURNtoContact(ctx, msg.Channel(), contact, realIDURN)
+				if err != nil {
+					log.WithError("Message Send Error", errors.Errorf("unable to add real Instagram URN %s to contact with uuid %s", realURN.String(), contact.UUID()))
+				}
+				referralIDExtURN, err := urns.NewURNFromParts(urns.ExternalScheme, referralID, "", "")
+				if err != nil {
+					log.WithError("Message Send Error", errors.Errorf("unable to make ext urn from %s", referralID))
+				}
+				extURN, err := h.Backend().AddURNtoContact(ctx, msg.Channel(), contact, referralIDExtURN)
+				if err != nil {
+					log.WithError("Message Send Error", errors.Errorf("unable to add URN %s to contact with uuid %s", extURN.String(), contact.UUID()))
+				}
+
+				referralInstagramURN, err := h.Backend().RemoveURNfromContact(ctx, msg.Channel(), contact, msg.URN())
+				if err != nil {
+					log.WithError("Message Send Error", errors.Errorf("unable to remove referral Instagram URN %s from contact with uuid %s", referralInstagramURN.String(), contact.UUID()))
+				}
+
+			}
+
+		}
+
+		// this was wired successfully
+		status.SetStatus(courier.MsgWired)
+	}
+
+	return status, nil
+}
+
+// DescribeURN looks up URN metadata for new contacts
+func (h *handler) DescribeURN(ctx context.Context, channel courier.Channel, urn urns.URN) (map[string]string, error) {
+	// can't do anything with Instagram refs, ignore them
+	if urn.IsInstagramRef() {
+		return map[string]string{}, nil
+	}
+
+	accessToken := channel.StringConfigForKey(courier.ConfigAuthToken, "")
+	if accessToken == "" {
+		return nil, fmt.Errorf("missing access token")
+	}
+
+	// build a request to lookup the stats for this contact
+	base, _ := url.Parse(graphURL)
+	path, _ := url.Parse(fmt.Sprintf("/%s", urn.Path()))
+	u := base.ResolveReference(path)
+
+	query := url.Values{}
+	query.Set("fields", "first_name,last_name")
+	query.Set("access_token", accessToken)
+	u.RawQuery = query.Encode()
+	req, _ := http.NewRequest(http.MethodGet, u.String(), nil)
+	rr, err := utils.MakeHTTPRequest(req)
+	if err != nil {
+		return nil, fmt.Errorf("unable to look up contact data:%s\n%s", err, rr.Response)
+	}
+
+	// read our first and last name
+	firstName, _ := jsonparser.GetString(rr.Body, "first_name")
+	lastName, _ := jsonparser.GetString(rr.Body, "last_name")
+
+	return map[string]string{"name": utils.JoinNonEmpty(" ", firstName, lastName)}, nil
+}
+
+// see https://developers.facebook.com/docs/messenger-platform/webhook#security
+func (h *handler) validateSignature(r *http.Request) error {
+	headerSignature := r.Header.Get(signatureHeader)
+	if headerSignature == "" {
+		return fmt.Errorf("missing request signature")
+	}
+	appSecret := h.Server().Config().FacebookApplicationSecret
+
+	body, err := handlers.ReadBody(r, 100000)
+	if err != nil {
+		return fmt.Errorf("unable to read request body: %s", err)
+	}
+
+	expectedSignature, err := fbCalculateSignature(appSecret, body)
+	if err != nil {
+		return err
+	}
+
+	signature := ""
+	if len(headerSignature) == 45 && strings.HasPrefix(headerSignature, "sha1=") {
+		signature = strings.TrimPrefix(headerSignature, "sha1=")
+	}
+
+	// compare signatures in way that isn't sensitive to a timing attack
+	if !hmac.Equal([]byte(expectedSignature), []byte(signature)) {
+		return fmt.Errorf("invalid request signature, expected: %s got: %s for body: '%s'", expectedSignature, signature, string(body))
+	}
+
+	return nil
+}
+
+func fbCalculateSignature(appSecret string, body []byte) (string, error) {
+	var buffer bytes.Buffer
+	buffer.Write(body)
+
+	// hash with SHA1
+	mac := hmac.New(sha1.New, []byte(appSecret))
+	mac.Write(buffer.Bytes())
+
+	return hex.EncodeToString(mac.Sum(nil)), nil
+}
