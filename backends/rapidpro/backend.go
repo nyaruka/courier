8b0131886c730175831bd6b0b12a69c3b7a9f155
Rowan Seymour <rowanseymour@gmail.com>
2022-01-19 09:10:22 -0500

    Allow more active redis connections

--- backends/rapidpro/backend.go
+++ backends/rapidpro/backend.go
@@ -592,11 +592,11 @@ func (b *backend) Start() error {
 	// create our pool
 	redisPool := &redis.Pool{
 		Wait:        true,              // makes callers wait for a connection
-		MaxActive:   8,                 // only open this many concurrent connections at once
+		MaxActive:   36,                // only open this many concurrent connections at once
 		MaxIdle:     4,                 // only keep up to this many idle
 		IdleTimeout: 240 * time.Second, // how long to wait before reaping a connection
 		Dial: func() (redis.Conn, error) {
-			conn, err := redis.Dial("tcp", fmt.Sprintf("%s", redisURL.Host))
+			conn, err := redis.Dial("tcp", redisURL.Host)
 			if err != nil {
 				return nil, err
 			}
