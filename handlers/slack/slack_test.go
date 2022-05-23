e8b18e9047b9a4ca46040f34fad1b397ba402f95
Rafael Soares <rafael.dvlpr@gmail.com>
2022-05-23 15:09:27 -0300

    test verification and receiveEvent

--- handlers/slack/slack_test.go
+++ handlers/slack/slack_test.go
@@ -8,16 +8,52 @@ import (
 	. "github.com/nyaruka/courier/handlers"
 )
 
+const (
+	channelUUID = "8eb23e93-5ecb-45ba-b726-3b064e0c568c"
+	receiveURL  = "/c/sl/" + channelUUID + "/receive/"
+)
+
 var testChannels = []courier.Channel{
-	courier.NewMockChannel("8eb23e93-5ecb-45ba-b726-3b064e0c56ab", "SL", "2022", "US", map[string]interface{}{"auth_token": "abc123"}),
+	courier.NewMockChannel(channelUUID, "SL", "2022", "US", map[string]interface{}{"bot_token": "xoxb-abc123"}),
 }
 
-var helloMsg = `{
-	
+const helloMsg = `{
+	"token": "one-long-verification-token",
+	"team_id": "T061EG9R6",
+	"api_app_id": "A0PNCHHK2",
+	"event": {
+			"type": "message",
+			"channel": "C0123ABCDEF",
+			"user": "U0123ABCDEF",
+			"text": "Hello World!",
+			"ts": "1355517523.000005",
+			"event_ts": "1355517523.000005",
+			"channel_type": "channel"
+	},
+	"type": "event_callback",
+	"authed_teams": [
+			"T061EG9R6"
+	],
+	"event_id": "Ev0PV52K21",
+	"event_time": 1355517523
 }`
 
 func setSendUrl(s *httptest.Server, h courier.ChannelHandler, c courier.Channel, m courier.Msg) {
 	apiURL = s.URL
+}
+
+var testCases = []ChannelHandleTestCase{
+	{
+		Label:      "Receive Hello Msg",
+		URL:        receiveURL,
+		Headers:    map[string]string{},
+		Data:       helloMsg,
+		URN:        Sp("slack:U0123ABCDEF"),
+		Text:       Sp("Hello World!"),
+		Status:     200,
+		Response:   "Accepted",
+		ExternalID: Sp("Ev0PV52K21"),
+	},
 }
 
 var defaultSendTestCases = []ChannelSendTestCase{
@@ -50,12 +86,24 @@ var defaultSendTestCases = []ChannelSendTestCase{
 	},
 }
 
-func TestSending(t *testing.T) {
-	var defaultChannel = courier.NewMockChannel("8eb23e93-5ecb-45ba-b726-3b064e0c56ab", "SL", "2022", "US",
-		map[string]interface{}{
-			configBotToken: "xoxb-123456789...",
-		},
-	)
+func TestHandler(t *testing.T) {
+	RunChannelTestCases(t, testChannels, newHandler(), testCases)
+}
 
-	RunChannelSendTestCases(t, defaultChannel, newHandler(), defaultSendTestCases, nil)
+func TestSending(t *testing.T) {
+	RunChannelSendTestCases(t, testChannels[0], newHandler(), defaultSendTestCases, nil)
+}
+
+func TestVerification(t *testing.T) {
+	RunChannelTestCases(t, testChannels, newHandler(), []ChannelHandleTestCase{
+		{Label: "Valid token", URL: receiveURL, Status: 200,
+			Data:     `{"token":"xoxb-abc123","challenge":"challenge123","type":"url_verification"}`,
+			Headers:  map[string]string{"content-type": "text/plain"},
+			Response: "challenge123", NoQueueErrorCheck: true, NoInvalidChannelCheck: true,
+		},
+		{Label: "Invalid token", URL: receiveURL, Status: 403,
+			Data:    `{"token":"abc321","challenge":"challenge123","type":"url_verification"}`,
+			Headers: map[string]string{"content-type": "text/plain"},
+		},
+	})
 }
