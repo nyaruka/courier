1ff03fc90ace37c630dafb3e15c81f4e23667c27
Rafael Soares <rafael.dvlpr@gmail.com>
2022-06-22 12:09:59 -0300

    tweaks slack channel tests and handler

--- handlers/slack/slack.go
+++ handlers/slack/slack.go
@@ -54,7 +54,7 @@ func handleURLVerification(ctx context.Context, channel courier.Channel, w http.
 	validationToken := channel.ConfigForKey(configValidationToken, "")
 	if validationToken != payload.Token {
 		w.WriteHeader(http.StatusForbidden)
-		return nil, fmt.Errorf("Wrong validation token for channel: %s", channel.UUID())
+		return nil, fmt.Errorf("wrong validation token for channel: %s", channel.UUID())
 	}
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusOK)
@@ -74,25 +74,17 @@ func (h *handler) receiveEvent(ctx context.Context, channel courier.Channel, w h
 	}
 
 	// if event is not a message or is from the bot ignore it
-	if strings.Contains(payload.Event.Type, "message") && payload.Event.BotID == "" {
+	if payload.Event.Type == "message" && payload.Event.BotID == "" && payload.Event.ChannelType == "im" {
 
 		date := time.Unix(int64(payload.EventTime), 0)
 
-		var userName string
-		var path string
-		if payload.Event.ChannelType == "channel" { //if is a message from a slack channel that bot is in
-			path = payload.Event.Channel
-		} else if payload.Event.ChannelType == "im" { // if is a direct message from a user
-			path = payload.Event.User
-			userInfo, log, err := getUserInfo(payload.Event.User, channel)
-			if err != nil {
-				h.Backend().WriteChannelLogs(ctx, []*courier.ChannelLog{log})
-				return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, err)
-			}
-			userName = userInfo.User.RealName
+		userInfo, log, err := getUserInfo(payload.Event.User, channel)
+		if err != nil {
+			h.Backend().WriteChannelLogs(ctx, []*courier.ChannelLog{log})
+			return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, err)
 		}
 
-		urn, err := urns.NewURNFromParts(urns.SlackScheme, path, "", userName)
+		urn, err := urns.NewURNFromParts(urns.SlackScheme, payload.Event.User, "", "")
 		if err != nil {
 			return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, err)
 		}
@@ -108,7 +100,7 @@ func (h *handler) receiveEvent(ctx context.Context, channel courier.Channel, w h
 		}
 
 		text := payload.Event.Text
-		msg := h.Backend().NewIncomingMsg(channel, urn, text).WithReceivedOn(date).WithExternalID(payload.EventID).WithContactName(userName)
+		msg := h.Backend().NewIncomingMsg(channel, urn, text).WithReceivedOn(date).WithExternalID(payload.EventID).WithContactName(userInfo.User.RealName)
 
 		for _, attURL := range attachmentURLs {
 			msg.WithAttachment(attURL)
@@ -338,125 +330,61 @@ func getUserInfo(userSlackID string, channel courier.Channel) (*UserInfo, *couri
 	return uInfo, nil, nil
 }
 
-// mtPayload is a struct that represents the body of a SendMmsg text part
+// mtPayload is a struct that represents the body of a SendMmsg text part.
+// https://api.slack.com/methods/chat.postMessage
 type mtPayload struct {
 	Channel string `json:"channel"`
 	Text    string `json:"text"`
 }
 
-// moPayload is a struct that represents message payload from message type event
+// moPayload is a struct that represents message payload from message type event.
+// https://api.slack.com/events/message.im
 type moPayload struct {
-	Token    string `json:"token,omitempty"`
-	TeamID   string `json:"team_id,omitempty"`
-	APIAppID string `json:"api_app_id,omitempty"`
-	Event    struct {
+	Token string `json:"token,omitempty"`
+	Event struct {
 		Type        string `json:"type,omitempty"`
 		Channel     string `json:"channel,omitempty"`
 		User        string `json:"user,omitempty"`
 		Text        string `json:"text,omitempty"`
-		Ts          string `json:"ts,omitempty"`
-		EventTs     string `json:"event_ts,omitempty"`
 		ChannelType string `json:"channel_type,omitempty"`
 		Files       []File `json:"files"`
 		BotID       string `json:"bot_id,omitempty"`
 	} `json:"event,omitempty"`
-	Type           string   `json:"type,omitempty"`
-	AuthedUsers    []string `json:"authed_users,omitempty"`
-	AuthedTeams    []string `json:"authed_teams,omitempty"`
-	Authorizations []struct {
-		EnterpriseID string `json:"enterprise_id,omitempty"`
-		TeamID       string `json:"team_id,omitempty"`
-		UserID       string `json:"user_id,omitempty"`
-		IsBot        bool   `json:"is_bot,omitempty"`
-	} `json:"authorizations,omitempty"`
-	EventContext string `json:"event_context,omitempty"`
-	EventID      string `json:"event_id,omitempty"`
-	EventTime    int    `json:"event_time,omitempty"`
-	Challenge    string `json:"challenge,omitempty"`
+	Type      string `json:"type,omitempty"`
+	EventID   string `json:"event_id,omitempty"`
+	EventTime int    `json:"event_time,omitempty"`
+	Challenge string `json:"challenge,omitempty"`
 }
 
 // File is a struct that represents file item that can be present in Files list in message event, or in FileResponse or in FileParams
 type File struct {
 	ID                 string `json:"id"`
-	Created            int    `json:"created"`
-	Timestamp          int    `json:"timestamp"`
-	Name               string `json:"name"`
-	Title              string `json:"title"`
 	Mimetype           string `json:"mimetype"`
-	Filetype           string `json:"filetype"`
-	PrettyType         string `json:"pretty_type"`
-	User               string `json:"user"`
-	Editable           bool   `json:"editable"`
-	Size               int    `json:"size"`
-	Mode               string `json:"mode"`
-	IsExternal         bool   `json:"is_external"`
-	ExternalType       string `json:"external_type"`
-	IsPublic           bool   `json:"is_public"`
-	PublicURLShared    bool   `json:"public_url_shared"`
-	DisplayAsBot       bool   `json:"display_as_bot"`
-	Username           string `json:"username"`
-	URLPrivate         string `json:"url_private"`
 	URLPrivateDownload string `json:"url_private_download"`
-	MediaDisplayType   string `json:"media_display_type"`
-	Thumb64            string `json:"thumb_64"`
-	Thumb80            string `json:"thumb_80"`
-	Thumb360           string `json:"thumb_360"`
-	Thumb360W          int    `json:"thumb_360_w"`
-	Thumb360H          int    `json:"thumb_360_h"`
-	Thumb160           string `json:"thumb_160"`
-	OriginalW          int    `json:"original_w"`
-	OriginalH          int    `json:"original_h"`
-	ThumbTiny          string `json:"thumb_tiny"`
-	Permalink          string `json:"permalink"`
 	PermalinkPublic    string `json:"permalink_public"`
-	HasRichPreview     bool   `json:"has_rich_preview"`
 }
 
-// FileResponse is a struct that represents the response from a request in files.sharedPublicURL to make public and shareable a file that is sent in a message, more information see https://api.slack.com/methods/files.sharedPublicURL.
+// FileResponse is a struct that represents the response from a request in files.sharedPublicURL to make public and shareable a file that is sent in a message.
+// https://api.slack.com/methods/files.sharedPublicURL.
 type FileResponse struct {
 	OK    bool   `json:"ok"`
 	File  File   `json:"file"`
 	Error string `json:"error"`
 }
 
-// FileParams is a struct that represents the request params send to slack api files.upload method to send a file to a channel conversation or a direct message conversation with a user, more
-// information see https://api.slack.com/methods/files.upload.
+// FileParams is a struct that represents the request params send to slack api files.upload method to send a file to conversation.
+// https://api.slack.com/methods/files.upload.
 type FileParams struct {
 	File     []byte `json:"file,omitempty"`
 	FileName string `json:"filename,omitempty"`
 	Channels string `json:"channels,omitempty"`
 }
 
-// UserInfo is a struct that represents the response from request in users.info slack api method, more information see https://api.slack.com/methods/users.info.
+// UserInfo is a struct that represents the response from request in users.info slack api method.
+// https://api.slack.com/methods/users.info.
 type UserInfo struct {
 	Ok   bool `json:"ok"`
 	User struct {
-		ID       string `json:"id"`
-		TeamID   string `json:"team_id"`
-		Name     string `json:"name"`
-		Deleted  bool   `json:"deleted"`
-		Color    string `json:"color"`
 		RealName string `json:"real_name"`
-		Tz       string `json:"tz"`
-		TzLabel  string `json:"tz_label"`
-		TzOffset int    `json:"tz_offset"`
-		Profile  struct {
-			AvatarHash            string `json:"avatar_hash"`
-			StatusText            string `json:"status_text"`
-			StatusEmoji           string `json:"status_emoji"`
-			RealName              string `json:"real_name"`
-			DisplayName           string `json:"display_name"`
-			RealNameNormalized    string `json:"real_name_normalized"`
-			DisplayNameNormalized string `json:"display_name_normalized"`
-			Email                 string `json:"email"`
-			ImageOriginal         string `json:"image_original"`
-			Image24               string `json:"image_24"`
-			Image32               string `json:"image_32"`
-			Image48               string `json:"image_48"`
-			Image72               string `json:"image_72"`
-			Image192              string `json:"image_192"`
-			Image512              string `json:"image_512"`
-			Team                  string `json:"team"`
-		} `json:"profile"`
 	} `json:"user"`
 }
