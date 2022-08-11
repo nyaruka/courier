4952749970849568dc76c680e04ba14d0d97292a
Rowan Seymour <rowanseymour@gmail.com>
2022-08-11 09:37:49 -0500

    Rename S3MediaBucket to S3AttachmentsBucket and S3MediaPrefix to S3AttachmentsPrefix

--- config.go
+++ config.go
@@ -15,15 +15,15 @@ type Config struct {
 	Redis     string `help:"URL describing how to connect to Redis"`
 	SpoolDir  string `help:"the local directory where courier will write statuses or msgs that need to be retried (needs to be writable)"`
 
-	AWSAccessKeyID     string `help:"the access key id to use when authenticating S3"`
-	AWSSecretAccessKey string `help:"the secret access key id to use when authenticating S3"`
-	AWSUseCredChain    bool   `help:"whether to use the AWS credentials chain. Defaults to false."`
-	S3Endpoint         string `help:"the S3 endpoint we will write attachments to"`
-	S3Region           string `help:"the S3 region we will write attachments to"`
-	S3MediaBucket      string `help:"the S3 bucket we will write attachments to"`
-	S3MediaPrefix      string `help:"the prefix that will be added to attachment filenames"`
-	S3DisableSSL       bool   `help:"whether we disable SSL when accessing S3. Should always be set to False unless you're hosting an S3 compatible service within a secure internal network"`
-	S3ForcePathStyle   bool   `help:"whether we force S3 path style. Should generally need to default to False unless you're hosting an S3 compatible service"`
+	AWSAccessKeyID      string `help:"the access key id to use when authenticating S3"`
+	AWSSecretAccessKey  string `help:"the secret access key id to use when authenticating S3"`
+	AWSUseCredChain     bool   `help:"whether to use the AWS credentials chain. Defaults to false."`
+	S3Endpoint          string `help:"the S3 endpoint we will write attachments to"`
+	S3Region            string `help:"the S3 region we will write attachments to"`
+	S3AttachmentsBucket string `help:"the S3 bucket we will write attachments to"`
+	S3AttachmentsPrefix string `help:"the prefix that will be added to attachment filenames"`
+	S3DisableSSL        bool   `help:"whether we disable SSL when accessing S3. Should always be set to False unless you're hosting an S3 compatible service within a secure internal network"`
+	S3ForcePathStyle    bool   `help:"whether we force S3 path style. Should generally need to default to False unless you're hosting an S3 compatible service"`
 
 	FacebookApplicationSecret    string `help:"the Facebook app secret"`
 	FacebookWebhookSecret        string `help:"the secret for Facebook webhook URL verification"`
@@ -56,15 +56,15 @@ func NewConfig() *Config {
 		Redis:    "redis://localhost:6379/15",
 		SpoolDir: "/var/spool/courier",
 
-		AWSAccessKeyID:     "",
-		AWSSecretAccessKey: "",
-		AWSUseCredChain:    false,
-		S3Endpoint:         "https://s3.amazonaws.com",
-		S3Region:           "us-east-1",
-		S3MediaBucket:      "courier-media",
-		S3MediaPrefix:      "/media/",
-		S3DisableSSL:       false,
-		S3ForcePathStyle:   false,
+		AWSAccessKeyID:      "",
+		AWSSecretAccessKey:  "",
+		AWSUseCredChain:     false,
+		S3Endpoint:          "https://s3.amazonaws.com",
+		S3Region:            "us-east-1",
+		S3AttachmentsBucket: "courier-media",
+		S3AttachmentsPrefix: "/media/",
+		S3DisableSSL:        false,
+		S3ForcePathStyle:    false,
 
 		FacebookApplicationSecret:    "missing_facebook_app_secret",
 		FacebookWebhookSecret:        "missing_facebook_webhook_secret",
