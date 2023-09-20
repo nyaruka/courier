6c4ab5c884a3d1c181c6722ae23143583459a68c
Rowan Seymour <rowanseymour@gmail.com>
2023-09-20 14:16:28 -0500

    Simplfy handlers splitting up messages

--- handlers/utils.go
+++ handlers/utils.go
@@ -11,6 +11,7 @@ import (
 	"github.com/nyaruka/courier"
 	"github.com/nyaruka/courier/utils"
 	"github.com/nyaruka/gocommon/urns"
+	"golang.org/x/exp/slices"
 )
 
 var (
@@ -92,15 +93,76 @@ func DecodePossibleBase64(original string) string {
 	return decoded
 }
 
-// SplitMsgByChannel splits the passed in string into segments that are at most channel config max length or type max length
+type MsgPartType int
+
+const (
+	MsgPartTypeText MsgPartType = iota
+	MsgPartTypeAttachment
+	MsgPartTypeCaptionedAttachment
+)
+
+// MsgPart represents a message part - either Text or Attachment will be set
+type MsgPart struct {
+	Type       MsgPartType
+	Text       string
+	Attachment string
+	IsFirst    bool
+	IsLast     bool
+}
+
+type SplitOptions struct {
+	MaxTextLen    int
+	MaxCaptionLen int
+	Captionable   []MediaType
+}
+
+// SplitMsg splits an outgoing message into separate text and attachment parts, with attachment parts first.
+// TODO combine with ResolveMedia
+func SplitMsg(m courier.MsgOut, opts SplitOptions) []MsgPart {
+	text := m.Text()
+	attachments := m.Attachments()
+	parts := make([]MsgPart, 0, 5)
+	asCaption := false
+
+	// if we have a single attachment and text we may be able to combine them into a captioned attachment
+	if len(attachments) == 1 && len(text) > 0 && (len(text) <= opts.MaxCaptionLen || opts.MaxCaptionLen == 0) {
+		att := attachments[0]
+		mediaType, _ := SplitAttachment(att)
+		mediaType = strings.Split(mediaType, "/")[0]
+		if slices.Contains(opts.Captionable, MediaType(mediaType)) {
+			parts = append(parts, MsgPart{Type: MsgPartTypeCaptionedAttachment, Text: text, Attachment: attachments[0]})
+			asCaption = true
+		}
+	}
+
+	if !asCaption {
+		for _, a := range attachments {
+			parts = append(parts, MsgPart{Type: MsgPartTypeAttachment, Attachment: a})
+		}
+		for _, t := range SplitMsgByChannel(m.Channel(), text, opts.MaxTextLen) {
+			if len(t) > 0 {
+				parts = append(parts, MsgPart{Type: MsgPartTypeText, Text: t})
+			}
+		}
+	}
+
+	if len(parts) > 0 {
+		parts[0].IsFirst = true
+		parts[len(parts)-1].IsLast = true
+	}
+
+	return parts
+}
+
+// deprecated use SplitMsg instead
 func SplitMsgByChannel(channel courier.Channel, text string, maxLength int) []string {
 	max := channel.IntConfigForKey(courier.ConfigMaxLength, maxLength)
 
-	return SplitMsg(text, max)
+	return SplitText(text, max)
 }
 
-// SplitMsg splits the passed in string into segments that are at most max length
-func SplitMsg(text string, max int) []string {
+// SplitText splits the passed in string into segments that are at most max length
+func SplitText(text string, max int) []string {
 	// smaller than our max, just return it
 	if len(text) <= max {
 		return []string{text}
