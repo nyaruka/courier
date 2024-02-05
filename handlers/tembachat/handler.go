0261a5b90c98f6003cb06596cd40366f6281e2b0
Rowan Seymour <rowanseymour@gmail.com>
2024-02-05 10:34:44 -0500

    Tweak temba-chat payload to support multiple events per request

--- handlers/tembachat/handler.go
+++ handlers/tembachat/handler.go
@@ -30,45 +30,54 @@ func newHandler() courier.ChannelHandler {
 // Initialize is called by the engine once everything is loaded
 func (h *handler) Initialize(s courier.Server) error {
 	h.SetServer(s)
-	s.AddHandlerRoute(h, http.MethodPost, "receive", courier.ChannelLogTypeMsgReceive, handlers.JSONPayload(h, h.receiveMessage))
+	s.AddHandlerRoute(h, http.MethodPost, "receive", courier.ChannelLogTypeMultiReceive, handlers.JSONPayload(h, h.receive))
 	return nil
 }
 
 type receivePayload struct {
-	Type string `json:"type" validate:"required"`
-	Msg  struct {
-		ChatID string `json:"chat_id"`
-		Text   string `json:"text"`
-	} `json:"msg"`
-	Chat struct {
-		ChatID string `json:"chat_id"`
-	} `json:"chat"`
+	ChatID string `json:"chat_id" validate:"required"`
+	Events []struct {
+		Type string `json:"type" validate:"required"`
+		Msg  struct {
+			Text string `json:"text"`
+		} `json:"msg"`
+	}
 }
 
-// receiveMessage is our HTTP handler function for incoming messages
-func (h *handler) receiveMessage(ctx context.Context, c courier.Channel, w http.ResponseWriter, r *http.Request, payload *receivePayload, clog *courier.ChannelLog) ([]courier.Event, error) {
-	if payload.Type == "msg_in" {
-		urn, err := urns.NewURNFromParts(urns.WebChatScheme, payload.Msg.ChatID, "", "")
-		if err != nil {
-			return nil, handlers.WriteAndLogRequestError(ctx, h, c, w, r, err)
-		}
-
-		msg := h.Backend().NewIncomingMsg(c, urn, payload.Msg.Text, "", clog)
-		return handlers.WriteMsgsAndResponse(ctx, h, []courier.MsgIn{msg}, w, r, clog)
-	} else if payload.Type == "chat_started" {
-		urn, err := urns.NewURNFromParts(urns.WebChatScheme, payload.Chat.ChatID, "", "")
-		if err != nil {
-			return nil, handlers.WriteAndLogRequestError(ctx, h, c, w, r, err)
-		}
-
-		evt := h.Backend().NewChannelEvent(c, courier.EventTypeNewConversation, urn, clog)
-
-		if err := h.Backend().WriteChannelEvent(ctx, evt, clog); err != nil {
-			return nil, err
-		}
-		return []courier.Event{evt}, courier.WriteChannelEventSuccess(w, evt)
+// receiveMessage is our HTTP handler function for incoming events
+func (h *handler) receive(ctx context.Context, c courier.Channel, w http.ResponseWriter, r *http.Request, payload *receivePayload, clog *courier.ChannelLog) ([]courier.Event, error) {
+	urn, err := urns.NewURNFromParts(urns.WebChatScheme, payload.ChatID, "", "")
+	if err != nil {
+		return nil, handlers.WriteAndLogRequestError(ctx, h, c, w, r, err)
 	}
-	return nil, handlers.WriteAndLogRequestIgnored(ctx, h, c, w, r, "")
+
+	events := make([]courier.Event, 0, 2)
+	data := make([]any, 0, 2)
+
+	for _, event := range payload.Events {
+		if event.Type == "msg_in" {
+			msg := h.Backend().NewIncomingMsg(c, urn, event.Msg.Text, "", clog)
+
+			if err = h.Backend().WriteMsg(ctx, msg, clog); err != nil {
+				return nil, err
+			}
+
+			events = append(events, msg)
+			data = append(data, courier.NewMsgReceiveData(msg))
+
+		} else if event.Type == "chat_started" {
+			evt := h.Backend().NewChannelEvent(c, courier.EventTypeNewConversation, urn, clog)
+
+			if err := h.Backend().WriteChannelEvent(ctx, evt, clog); err != nil {
+				return nil, err
+			}
+
+			events = append(events, evt)
+			data = append(data, courier.NewEventReceiveData(evt))
+		}
+	}
+
+	return events, courier.WriteDataResponse(w, http.StatusOK, "Events Handled", data)
 }
 
 type sendPayload struct {
