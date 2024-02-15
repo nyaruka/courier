f6bf9c1c17beb6237aaed16c6011cb7b6f0f05f7
Norbert Kwizera <norkans7@gmail.com>
2024-02-15 16:23:26 +0200

    Use MockResponses and ExpectedRequests in WC tests

--- handlers/wechat/handler_test.go
+++ handlers/wechat/handler_test.go
@@ -292,59 +292,82 @@ func TestBuildAttachmentRequest(t *testing.T) {
 	assert.Len(t, clog.HTTPLogs(), 1)
 }
 
-// setSendURL takes care of setting the sendURL to call
-func setSendURL(s *httptest.Server, h courier.ChannelHandler, c courier.Channel, m courier.MsgOut) {
-	sendURL = s.URL
-}
-
 var defaultSendTestCases = []OutgoingTestCase{
 	{
-		Label:              "Plain Send",
-		MsgText:            "Simple Message ☺",
-		MsgURN:             "wechat:12345",
-		MockResponseStatus: 200,
-		ExpectedHeaders: map[string]string{
-			"Content-Type": "application/json",
-			"Accept":       "application/json",
+		Label:   "Plain Send",
+		MsgText: "Simple Message ☺",
+		MsgURN:  "wechat:12345",
+		MockResponses: map[string][]*httpx.MockResponse{
+			"https://api.weixin.qq.com/cgi-bin/message/custom/send*": {
+				httpx.NewMockResponse(200, nil, []byte(``)),
+			},
 		},
-		ExpectedRequestBody: `{"msgtype":"text","touser":"12345","text":{"content":"Simple Message ☺"}}`,
-		ExpectedMsgStatus:   "W",
-		SendPrep:            setSendURL,
+		ExpectedRequests: []ExpectedRequest{{
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+				"Accept":       "application/json",
+			},
+			Body: `{"msgtype":"text","touser":"12345","text":{"content":"Simple Message ☺"}}`,
+		}},
+		ExpectedMsgStatus: "W",
 	},
 	{
-		Label:              "Long Send",
-		MsgText:            "This is a longer message than 160 characters and will cause us to split it into two separate parts, isn't that right but it is even longer than before I say, I need to keep adding more things to make it work",
-		MsgURN:             "wechat:12345",
-		MockResponseStatus: 200,
-		ExpectedHeaders: map[string]string{
-			"Content-Type": "application/json",
-			"Accept":       "application/json",
+		Label:   "Long Send",
+		MsgText: "This is a longer message than 160 characters and will cause us to split it into two separate parts, isn't that right but it is even longer than before I say, I need to keep adding more things to make it work",
+		MsgURN:  "wechat:12345",
+		MockResponses: map[string][]*httpx.MockResponse{
+			"https://api.weixin.qq.com/cgi-bin/message/custom/send*": {
+				httpx.NewMockResponse(200, nil, []byte(``)),
+				httpx.NewMockResponse(200, nil, []byte(``)),
+			},
 		},
-		ExpectedRequestBody: `{"msgtype":"text","touser":"12345","text":{"content":"I need to keep adding more things to make it work"}}`,
-		ExpectedMsgStatus:   "W",
-		SendPrep:            setSendURL,
+		ExpectedRequests: []ExpectedRequest{
+			{
+				Headers: map[string]string{
+					"Content-Type": "application/json",
+					"Accept":       "application/json",
+				},
+				Body: `{"msgtype":"text","touser":"12345","text":{"content":"This is a longer message than 160 characters and will cause us to split it into two separate parts, isn't that right but it is even longer than before I say,"}}`,
+			},
+			{
+				Headers: map[string]string{
+					"Content-Type": "application/json",
+					"Accept":       "application/json",
+				},
+				Body: `{"msgtype":"text","touser":"12345","text":{"content":"I need to keep adding more things to make it work"}}`,
+			},
+		},
+		ExpectedMsgStatus: "W",
 	},
 	{
-		Label:              "Send Attachment",
-		MsgText:            "My pic!",
-		MsgURN:             "wechat:12345",
-		MsgAttachments:     []string{"image/jpeg:https://foo.bar/image.jpg"},
-		MockResponseStatus: 200,
-		ExpectedHeaders: map[string]string{
-			"Content-Type": "application/json",
-			"Accept":       "application/json",
+		Label:          "Send Attachment",
+		MsgText:        "My pic!",
+		MsgURN:         "wechat:12345",
+		MsgAttachments: []string{"image/jpeg:https://foo.bar/image.jpg"},
+		MockResponses: map[string][]*httpx.MockResponse{
+			"https://api.weixin.qq.com/cgi-bin/message/custom/send*": {
+				httpx.NewMockResponse(200, nil, []byte(``)),
+			},
 		},
-		ExpectedRequestBody: `{"msgtype":"text","touser":"12345","text":{"content":"My pic!\nhttps://foo.bar/image.jpg"}}`,
-		ExpectedMsgStatus:   "W",
-		SendPrep:            setSendURL,
+		ExpectedRequests: []ExpectedRequest{{
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+				"Accept":       "application/json",
+			},
+			Body: `{"msgtype":"text","touser":"12345","text":{"content":"My pic!\nhttps://foo.bar/image.jpg"}}`,
+		}},
+		ExpectedMsgStatus: "W",
 	},
 	{
-		Label:              "Error Sending",
-		MsgText:            "Error Message",
-		MsgURN:             "wechat:12345",
-		MockResponseStatus: 401,
-		ExpectedMsgStatus:  "E",
-		SendPrep:           setSendURL,
+		Label:   "Error Sending",
+		MsgText: "Error Message",
+		MsgURN:  "wechat:12345",
+		MockResponses: map[string][]*httpx.MockResponse{
+			"https://api.weixin.qq.com/cgi-bin/message/custom/send*": {
+				httpx.NewMockResponse(401, nil, []byte(`Error`)),
+			},
+		},
+		ExpectedMsgStatus: "E",
 	},
 }
 
