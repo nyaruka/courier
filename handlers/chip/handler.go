66d06219dda53536896f246cf31d265ec77c766c
Rowan Seymour <rowanseymour@gmail.com>
2024-05-29 11:23:51 -0500

    Set content-type header in chip handler

--- handlers/chip/handler.go
+++ handlers/chip/handler.go
@@ -140,6 +140,7 @@ func (h *handler) Send(ctx context.Context, msg courier.MsgOut, res *courier.Sen
 		},
 	}
 	req, _ := http.NewRequest("POST", sendURL+"/"+string(msg.Channel().UUID())+"/", bytes.NewReader(jsonx.MustMarshal(payload)))
+	req.Header.Set("Content-Type", "application/json")
 
 	resp, _, err := h.RequestHTTP(req, clog)
 	if err != nil || resp.StatusCode/100 == 5 {
