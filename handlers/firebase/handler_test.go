59627a76e60c464e7847f6042ffe122f47bf9956
Norbert Kwizera <norkans7@gmail.com>
2025-11-06 15:14:49 +0200

    Remove support for server API keys for FCM channels

--- handlers/firebase/handler_test.go
+++ handlers/firebase/handler_test.go
@@ -35,20 +35,6 @@ var testChannels = []courier.Channel{
 	test.NewMockChannel("8eb23e93-5ecb-45ba-b726-3b064e0c568c", "FCM", "1234", "",
 		[]string{urns.Firebase.Prefix},
 		map[string]any{
-			configKey:   "FCMKey",
-			configTitle: "FCMTitle",
-		}),
-	test.NewMockChannel("8eb23e93-5ecb-45ba-b726-3b064e0c568c", "FCM", "1234", "",
-		[]string{urns.Firebase.Prefix},
-		map[string]any{
-			configKey:          "FCMKey",
-			configNotification: true,
-			configTitle:        "FCMTitle",
-		}),
-
-	test.NewMockChannel("8eb23e93-5ecb-45ba-b726-3b064e0c568c", "FCM", "1234", "",
-		[]string{urns.Firebase.Prefix},
-		map[string]any{
 			configTitle: "FCMTitle",
 			configCredentialsFile: map[string]any{
 				"type":                        "service_account",
@@ -130,133 +116,6 @@ var testCases = []IncomingTestCase{
 
 func TestIncoming(t *testing.T) {
 	RunIncomingTestCases(t, testChannels, newHandler(), testCases)
-}
-
-var notificationSendAPIkeyTestCases = []OutgoingTestCase{
-	{
-		Label:      "Plain Send",
-		MsgText:    "Simple Message",
-		MsgURN:     "fcm:250788123123",
-		MsgURNAuth: "auth1",
-		MockResponses: map[string][]*httpx.MockResponse{
-			"https://fcm.googleapis.com/fcm/send": {
-				httpx.NewMockResponse(200, nil, []byte(`{"success":1, "multicast_id": 123456}`)),
-			},
-		},
-		ExpectedRequests: []ExpectedRequest{{
-			Headers: map[string]string{"Authorization": "key=FCMKey"},
-			Body:    `{"data":{"type":"rapidpro","title":"FCMTitle","message":"Simple Message","message_id":10,"session_status":""},"notification":{"title":"FCMTitle","body":"Simple Message"},"content_available":true,"to":"auth1","priority":"high"}`,
-		}},
-		ExpectedExtIDs: []string{"123456"},
-	},
-}
-
-var sendAPIkeyTestCases = []OutgoingTestCase{
-	{
-		Label:      "Plain Send",
-		MsgText:    "Simple Message",
-		MsgURN:     "fcm:250788123123",
-		MsgURNAuth: "auth1",
-		MockResponses: map[string][]*httpx.MockResponse{
-			"https://fcm.googleapis.com/fcm/send": {
-				httpx.NewMockResponse(200, nil, []byte(`{"success":1, "multicast_id": 123456}`)),
-			},
-		},
-		ExpectedRequests: []ExpectedRequest{{
-			Headers: map[string]string{"Authorization": "key=FCMKey"},
-			Body:    `{"data":{"type":"rapidpro","title":"FCMTitle","message":"Simple Message","message_id":10,"session_status":""},"content_available":false,"to":"auth1","priority":"high"}`,
-		}},
-		ExpectedExtIDs: []string{"123456"},
-	},
-	{
-		Label:      "Long Message",
-		MsgText:    longMsg,
-		MsgURN:     "fcm:250788123123",
-		MsgURNAuth: "auth1",
-		MockResponses: map[string][]*httpx.MockResponse{
-			"https://fcm.googleapis.com/fcm/send": {
-				httpx.NewMockResponse(200, nil, []byte(`{"success":1, "multicast_id": 123456}`)),
-				httpx.NewMockResponse(200, nil, []byte(`{"success":1, "multicast_id": 123456}`)),
-			},
-		},
-		ExpectedRequests: []ExpectedRequest{
-			{
-				Headers: map[string]string{"Authorization": "key=FCMKey"},
-				Body:    `{"data":{"type":"rapidpro","title":"FCMTitle","message":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas convallis augue vel placerat congue.\nEtiam nec tempus enim. Cras placerat at est vel suscipit. Duis quis faucibus metus, non elementum tortor.\nPellentesque posuere ullamcorper metus auctor venenatis. Proin eget hendrerit dui. Sed eget massa nec mauris consequat pretium.\nPraesent mattis arcu tortor, ac aliquet turpis tincidunt eu.\n\nFusce ut lacinia augue. Vestibulum felis nisi, porta ut est condimentum, condimentum volutpat libero.\nSuspendisse a elit venenatis, condimentum sem at, ultricies mauris. Morbi interdum sem id tempor tristique.\nUt tincidunt massa eu purus lacinia sodales a volutpat neque. Cras dolor quam, eleifend a rhoncus quis, sodales nec purus.\nVivamus justo dolor, gravida at quam eu, hendrerit rutrum justo. Sed hendrerit nisi vitae nisl ornare tristique.\nProin vulputate id justo non aliquet.\n\nDuis eu arcu pharetra, laoreet nunc at, pharetra sapien. Nulla eu libero diam.\nDonec euismod dapibus ligula, sit amet hendrerit neque vulput","message_id":10,"session_status":""},"content_available":false,"to":"auth1","priority":"high"}`,
-			},
-			{
-				Headers: map[string]string{"Authorization": "key=FCMKey"},
-				Body:    `{"data":{"type":"rapidpro","title":"FCMTitle","message":"ate ac.","message_id":10,"session_status":""},"content_available":false,"to":"auth1","priority":"high"}`,
-			},
-		},
-		ExpectedExtIDs: []string{"123456", "123456"},
-	},
-	{
-		Label:           "Quick Reply",
-		MsgText:         "Simple Message",
-		MsgURN:          "fcm:250788123123",
-		MsgURNAuth:      "auth1",
-		MsgQuickReplies: []models.QuickReply{{Text: "yes"}, {Text: "no"}},
-		MsgAttachments:  []string{"image/jpeg:https://foo.bar"},
-		MockResponses: map[string][]*httpx.MockResponse{
-			"https://fcm.googleapis.com/fcm/send": {
-				httpx.NewMockResponse(200, nil, []byte(`{"success":1, "multicast_id": 123456}`)),
-			},
-		},
-		ExpectedRequests: []ExpectedRequest{{
-			Headers: map[string]string{"Authorization": "key=FCMKey"},
-			Body:    `{"data":{"type":"rapidpro","title":"FCMTitle","message":"Simple Message\nhttps://foo.bar","message_id":10,"session_status":"","quick_replies":["yes","no"]},"content_available":false,"to":"auth1","priority":"high"}`,
-		}},
-		ExpectedExtIDs: []string{"123456"},
-	},
-	{
-		Label:      "Error",
-		MsgText:    "Error",
-		MsgURN:     "fcm:250788123123",
-		MsgURNAuth: "auth1",
-		MockResponses: map[string][]*httpx.MockResponse{
-			"https://fcm.googleapis.com/fcm/send": {
-				httpx.NewMockResponse(200, nil, []byte(`{ "success": 0 }`)),
-			},
-		},
-		ExpectedRequests: []ExpectedRequest{{
-			Headers: map[string]string{"Authorization": "key=FCMKey"},
-			Body:    `{"data":{"type":"rapidpro","title":"FCMTitle","message":"Error","message_id":10,"session_status":""},"content_available":false,"to":"auth1","priority":"high"}`,
-		}},
-		ExpectedError: courier.ErrResponseContent,
-	},
-	{
-		Label:      "No Multicast ID",
-		MsgText:    "Error",
-		MsgURN:     "fcm:250788123123",
-		MsgURNAuth: "auth1",
-		MockResponses: map[string][]*httpx.MockResponse{
-			"https://fcm.googleapis.com/fcm/send": {
-				httpx.NewMockResponse(200, nil, []byte(`{ "success": 1 }`)),
-			},
-		},
-		ExpectedRequests: []ExpectedRequest{{
-			Headers: map[string]string{"Authorization": "key=FCMKey"},
-			Body:    `{"data":{"type":"rapidpro","title":"FCMTitle","message":"Error","message_id":10,"session_status":""},"content_available":false,"to":"auth1","priority":"high"}`,
-		}},
-		ExpectedError: courier.ErrResponseContent,
-	},
-	{
-		Label:      "Request Error",
-		MsgText:    "Error",
-		MsgURN:     "fcm:250788123123",
-		MsgURNAuth: "auth1",
-		MockResponses: map[string][]*httpx.MockResponse{
-			"https://fcm.googleapis.com/fcm/send": {
-				httpx.NewMockResponse(500, nil, []byte(`{ "success": 0 }`)),
-			},
-		},
-		ExpectedRequests: []ExpectedRequest{{
-			Headers: map[string]string{"Authorization": "key=FCMKey"},
-			Body:    `{"data":{"type":"rapidpro","title":"FCMTitle","message":"Error","message_id":10,"session_status":""},"content_available":false,"to":"auth1","priority":"high"}`,
-		}},
-		ExpectedError: courier.ErrConnectionFailed,
-	},
 }
 
 var notificationSendTestCases = []OutgoingTestCase{
@@ -442,9 +301,7 @@ func setupBackend(mb *test.MockBackend) {
 }
 
 func TestOutgoing(t *testing.T) {
-	RunOutgoingTestCases(t, testChannels[0], newHandler(), sendAPIkeyTestCases, []string{"FCMKey"}, setupBackend)
-	RunOutgoingTestCases(t, testChannels[1], newHandler(), notificationSendAPIkeyTestCases, []string{"FCMKey"}, setupBackend)
 
-	RunOutgoingTestCases(t, testChannels[2], newHandler(), sendTestCases, []string{}, setupBackend)
-	RunOutgoingTestCases(t, testChannels[3], newHandler(), notificationSendTestCases, []string{}, setupBackend)
+	RunOutgoingTestCases(t, testChannels[0], newHandler(), sendTestCases, []string{}, setupBackend)
+	RunOutgoingTestCases(t, testChannels[1], newHandler(), notificationSendTestCases, []string{}, setupBackend)
 }
