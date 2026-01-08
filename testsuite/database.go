4984743dfb0fe8b8817ddb3e856125e228c67b19
Norbert Kwizera <norkans7@gmail.com>
2026-01-08 14:22:27 +0200

    Add support for msgs external identifier field

--- testsuite/database.go
+++ testsuite/database.go
@@ -13,34 +13,35 @@ import (
 )
 
 type DBMsg struct {
-	OrgID        models.OrgID         `db:"org_id"`
-	ID           int64                `db:"id"`
-	UUID         models.MsgUUID       `db:"uuid"`
-	Direction    models.MsgDirection  `db:"direction"`
-	Status       models.MsgStatus     `db:"status"`
-	MsgType      string               `db:"msg_type"`
-	Visibility   models.MsgVisibility `db:"visibility"`
-	HighPriority bool                 `db:"high_priority"`
-	IsAndroid    bool                 `db:"is_android"`
-	Text         string               `db:"text"`
-	Attachments  pq.StringArray       `db:"attachments"`
-	QuickReplies pq.StringArray       `db:"quick_replies"`
-	Locale       null.String          `db:"locale"`
-	Templating   *models.Templating   `db:"templating"`
-	ExternalID   null.String          `db:"external_id"`
-	ChannelID    models.ChannelID     `db:"channel_id"`
-	ContactID    models.ContactID     `db:"contact_id"`
-	ContactURNID models.ContactURNID  `db:"contact_urn_id"`
-	MsgCount     int                  `db:"msg_count"`
-	CreatedByID  null.Int             `db:"created_by_id"`
-	CreatedOn    time.Time            `db:"created_on"`
-	ModifiedOn   time.Time            `db:"modified_on"`
-	SentOn       *time.Time           `db:"sent_on"`
-	ErrorCount   int                  `db:"error_count"`
-	FailedReason null.String          `db:"failed_reason"`
-	NextAttempt  *time.Time           `db:"next_attempt"`
-	OptInID      null.Int             `db:"optin_id"`
-	LogUUIDs     pq.StringArray       `db:"log_uuids"`
+	OrgID              models.OrgID         `db:"org_id"`
+	ID                 int64                `db:"id"`
+	UUID               models.MsgUUID       `db:"uuid"`
+	Direction          models.MsgDirection  `db:"direction"`
+	Status             models.MsgStatus     `db:"status"`
+	MsgType            string               `db:"msg_type"`
+	Visibility         models.MsgVisibility `db:"visibility"`
+	HighPriority       bool                 `db:"high_priority"`
+	IsAndroid          bool                 `db:"is_android"`
+	Text               string               `db:"text"`
+	Attachments        pq.StringArray       `db:"attachments"`
+	QuickReplies       pq.StringArray       `db:"quick_replies"`
+	Locale             null.String          `db:"locale"`
+	Templating         *models.Templating   `db:"templating"`
+	ExternalID         null.String          `db:"external_id"`
+	ExternalIdentifier null.String          `db:"external_identifier"`
+	ChannelID          models.ChannelID     `db:"channel_id"`
+	ContactID          models.ContactID     `db:"contact_id"`
+	ContactURNID       models.ContactURNID  `db:"contact_urn_id"`
+	MsgCount           int                  `db:"msg_count"`
+	CreatedByID        null.Int             `db:"created_by_id"`
+	CreatedOn          time.Time            `db:"created_on"`
+	ModifiedOn         time.Time            `db:"modified_on"`
+	SentOn             *time.Time           `db:"sent_on"`
+	ErrorCount         int                  `db:"error_count"`
+	FailedReason       null.String          `db:"failed_reason"`
+	NextAttempt        *time.Time           `db:"next_attempt"`
+	OptInID            null.Int             `db:"optin_id"`
+	LogUUIDs           pq.StringArray       `db:"log_uuids"`
 }
 
 func ReadDBMsg(t *testing.T, rt *runtime.Runtime, uuid models.MsgUUID) *DBMsg {
