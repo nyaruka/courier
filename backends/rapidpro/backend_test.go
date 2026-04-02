549fcbc3713e9864bb8420e295d6a017b2c588a1
Norbert Kwizera <norkans7@gmail.com>
2026-04-02 17:31:39 +0200

    Fix TestMailroomEvents by resetting DB and pre-creating contact

--- backends/rapidpro/backend_test.go
+++ backends/rapidpro/backend_test.go
@@ -1256,17 +1256,22 @@ func (ts *BackendTestSuite) TestSessionTimeout() {
 func (ts *BackendTestSuite) TestMailroomEvents() {
 	ctx := context.Background()
 
+	testsuite.ResetDB(ts.T(), ts.b.rt)
 	testsuite.ResetValkey(ts.T(), ts.b.rt)
 
 	channel := ts.getChannel("KN", "dbc126ed-66bc-4e28-b67b-81dc3327c95d")
 	clog := courier.NewChannelLog(courier.ChannelLogTypeUnknown, channel, nil)
 	urn := urns.URN("tel:+12065551616")
 
+	// ensure contact exists before event write so new_contact is false
+	_, err := contactForURN(ctx, ts.b, channel.OrgID_, channel, urn, nil, "kermit frog", true, clog)
+	ts.NoError(err)
+
 	event := ts.b.NewChannelEvent(channel, models.EventTypeReferral, urn, clog).
 		WithExtra(map[string]string{"ref_id": "12345"}).
 		WithContactName("kermit frog").
 		WithOccurredOn(time.Date(2020, 8, 5, 13, 30, 0, 123456789, time.UTC))
-	err := ts.b.WriteChannelEvent(ctx, event, clog)
+	err = ts.b.WriteChannelEvent(ctx, event, clog)
 	ts.NoError(err)
 
 	contact, err := contactForURN(ctx, ts.b, channel.OrgID_, channel, urn, nil, "", true, clog)
